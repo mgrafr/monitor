a039ba3b4681cc45d7930989de93e79bd89d812a
domo-site <93765654+mgrafr@users.noreply.github.com>
2021-11-21 15:26:32 +0100

    modif fichiers index pour les écrans>768x1024

--- index.php
+++ index.php
@@ -1,40 +1,13 @@
 <?php
-// pour les variables de session----------------------
-session_start();
-include ("admin/config.php");$_SESSION["exeption_db"]="";
-// Check connection DB
-$conn = new mysqli(SERVEUR, UTILISATEUR, MOTDEPASSE, DBASE);
-if ($conn->connect_error) { $_SESSION["exeption_db"]="pas de connexion à la BD";}
-//
-// pour vérifier la connexion au net------------------
-if (!$sock = @fsockopen('www.google.fr', 80, $num, $error, 5)) 
-$_SESSION["TC"]="0";
-else 
-$_SESSION["TC"]="200";
-// -variables----------------------------------------
-$_SESSION["d_root"]=$_SERVER["DOCUMENT_ROOT"];
-$_SESSION["d_admin"]=$_SERVER["DOCUMENT_ROOT"]."/admin/";
-$_SESSION["d_include"]=$_SERVER["DOCUMENT_ROOT"]."/include/";
-$_SESSION["domaine"]=$_SERVER['HTTP_HOST'];
-// ----------------------------------------------------
-// début du programme
-include ("include/entete_html.php");// la partie <head de la page html
-include ("include/header.php");// l' affichage du menu de la page d'accueil
-include ("include/accueil.php");// l' affichage page accueil
-if (ON_MET==true) include ("include/meteo.php");	// une page de prévision météo
-include ("include/interieur.php");// plan intérieur
-if (ON_ALARM==true) include ("include/alarmes.php"); // alarmes absence et nuit
-if (ON_GRAPH==true) include ("include/graphiques.php");// édition de graphiques
-// autre pages disponibles à décommenter pour les inclure
-if (ON_EXT==true) include include ("include/exterieur.php");
-// include ("include/commandes.php");
-if (ON_ONOFF==true) include ("include/mur_inter.php");
-if (ON_APP==true) include ("include/app_diverses.php");
-include ("include/admin.php");// administration
-if (ON_ZIGBEE==true) include ("include/zigbee.php");// fronted zigbee2mqtt
-if (ON_MUR==true) include ("include/mur_cam.php");
-if (ON_DVR==true) include ("include/dvr.php");
-if (ON_NAGIOS==true) include ("include/nagios.php");//monitoring
-include ("include/footer.php");// fin de la page avec les scrpits JS
+echo 
+'<html><body style="background-color: cornsilk;"><script>
+var larg = screen.width;
+if (larg<769 ){ window.location.href="http://192.168.1.7/monitor/index_loc.php";}
+</script>';
+echo '
+<iframe id="inline_monitor"
+    title="Inline Frame Example"
+    style="width:768px;height:1024px;margin:0 30%;background-color: cornsilk;"
+    src="http://192.168.1.7/monitor/index_loc.php">
+</iframe></body></html>';
 ?>
-</body></html>
