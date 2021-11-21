65df6f91906437f25aa20c702d822b47cf33bdb3
domo-site <93765654+mgrafr@users.noreply.github.com>
2021-11-21 16:27:01 +0100

    ajouter commentaire

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
+// indiquer l'adresse du repertoire  du site sur le serveur
+$lien="http://192.168.1.7/monitor/monitor-main/index_loc.php";
 echo 
 '<html><body style="background-color: cornsilk;"><script>
 var larg = screen.width;
-if (larg<769 ){ window.location.href="http://192.168.1.7/monitor/index_loc.php";}
+if (larg<769 ){ window.location.href="'.$lien.';}
 </script>';
 echo '
 <iframe id="inline_monitor"
     title="Inline Frame Example"
     style="width:768px;height:1024px;margin:0 30%;background-color: cornsilk;"
-    src="http://192.168.1.7/monitor/index_loc.php">
+    src="'.$lien.'">
 </iframe></body></html>';
 ?>
