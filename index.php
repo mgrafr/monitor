642387a7fca7362413ce7a0db3cf022b5c07b296
domo-site <93765654+mgrafr@users.noreply.github.com>
2021-11-24 11:08:27 +0100

    modif pour acces distant

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php
 echo '<!DOCTYPE html><html><body style="background-color: cornsilk;">';
-$domaine=$_SERVER['HTTP_HOST'];
+
+$rep="/"; $domaine=$_SERVER['HTTP_HOST'];
+if ($domaine=="192.168.1.7") $rep="/monitor/";
 echo '
 <script>
 var larg = screen.width;
-if (larg<769 ){ window.location.href="'.$domaine.'/monitor/index_loc.php";}
+if (larg<769 ){ window.location.href="'.$rep.'index_loc.php";}
 </script>';
 echo '
 <iframe id="inline_monitor"
     style="width:768px;height:1024px;margin:0 30%;background-color: cornsilk;"
-    src="index_loc.php">
+    src="'.$rep.'index_loc.php">
 </iframe></body></html>';
 ?>
