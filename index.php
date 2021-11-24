53a630d943063e4020c07fd9811aff9420353e26
domo-site <93765654+mgrafr@users.noreply.github.com>
2021-11-24 10:06:31 +0100

    correction pour appel distant

--- index.php
+++ index.php
@@ -1,16 +1,14 @@
 <?php
-echo $_SERVER['HTTP_HOST'];
-// indiquer l'adresse du repertoire  du site sur le serveur
-$lien="http://192.168.1.7/monitor/monitor-main/index_loc.php";
-echo 
-'<html><body style="background-color: cornsilk;"><script>
+echo '<!DOCTYPE html><html><body style="background-color: cornsilk;">';
+$domaine=$_SERVER['HTTP_HOST'];
+echo '
+<script>
 var larg = screen.width;
-if (larg<769 ){ window.location.href="'.$lien.';}
+if (larg<769 ){ window.location.href="'.$domaine.'/monitor/index_loc.php";}
 </script>';
 echo '
 <iframe id="inline_monitor"
-    title="Inline Frame Example"
     style="width:768px;height:1024px;margin:0 30%;background-color: cornsilk;"
-    src="'.$lien.'">
+    src="index_loc.php">
 </iframe></body></html>';
 ?>
