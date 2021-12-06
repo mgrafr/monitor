97ff83acb3a13acb4846fd3091dfb384d1370e66
domo-site <93765654+mgrafr@users.noreply.github.com>
2021-12-06 08:12:28 +0100

    modif pour text1

--- include/test_pass.php
+++ include/test_pass.php
@@ -7,7 +7,7 @@ session_start();
 			  else {$tt=0;echo "<p id='mp1' style='float:left;'>entrer mot de passe - </p>";}
 			  if (isset($_SESSION['passworda']) && (($_SESSION['passworda']) != PWDALARM)){$tt=0;echo "mot de passe non valide - ";}
 			  else {echo "<script>text1='pwd:ok';</script>";$style1="block";}
-			  if ($tt<time()) {$tt=0;echo "<p id='mp2' >temps pwd dépassé - </p>";}
+			  if ($tt<time()) {$tt=0;echo "<p id='mp2' >temps pwd dépassé - </p><script>text1='pwd:renouveler';</script>";}
    			  if ($tt==0){	
 				  echo '<div id='.$id_pec.'"><button type="button" id="'.$id_button.'" style="background-color: #4d4d4d;
 border-color: #e0e3e6;border-radius: 0.55rem" class="btn btn-primary"  data-toggle="modal" data-target="#pwdalarm">
