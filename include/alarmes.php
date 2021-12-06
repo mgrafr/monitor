0def01673cde02dd88a621a5dee607328efddfd0
domo-site <93765654+mgrafr@users.noreply.github.com>
2021-12-06 11:47:30 +0100

    ajouté info

--- include/alarmes.php
+++ include/alarmes.php
@@ -15,14 +15,14 @@ session_start();
 				</div>
 				</div>
  </div></div><svg version="1.1" id="zm" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-	 viewBox="0 0 326 181" style="position:relative;top:-10px;left:20px" xml:space="preserve">
+	 viewBox="0 0 326 18" style="width:500px" xml:space="preserve">
 <style type="text/css">
 	.st208{fill:#03A8F3;}
 	.st207{font-size:13.5px;}
 </style><a id="zm" href="#alarmes">
 <rect x="0.9" y="-0.7" class="st208" width="31.2" height="18.8"/>
 <text transform="matrix(1 0 0 1 5.4312 13.3434)" class="st203 st33 st207">Z M</text></a>
-</svg>
+</svg> <p style="position: relative;top: -190px;left: 100px;">Pour afficher la liste des caméras possible Modect<br>et pour demander un jeton pour Modect</p>
  <div class="modal" role="dialog" id="pwdalarm">
                         
 		    <div id="verif_mpa" >
