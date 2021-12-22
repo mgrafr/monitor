fe44fe9ed5c72e527c32728f0136e96aa6f94550
domo-site <93765654+mgrafr@users.noreply.github.com>
2021-12-22 15:59:36 +0100

    add

---
+++ scripts_dz/lua/string_tableaux.lua
@@ -0,0 +1,12 @@
+-- liste de variables
+-- string
+jour_poubelle_grise="Wednesday"
+jour_poubelle_jaune="Sunday"
+semaine_poub_jaune= 0  -- 0 pour pair 1 pour impair
+jour_fosse="21"
+-- anniversaires
+anniversaires = {["17-09"]="Damien",["18-05"]="Yoann",["14-09"]="Jonathan",["19-07"]="Alexandra",["25-08"]="Charlotte",["01-05"]="Guillaume",
+["07-11"]="Corentin",["22-08"]="Pauline",["14-03"]="Clémence",["31-10"]="Eric",["01-02"]="Nathalie",["14-04"]="Christèle",["25-04"]="Katy",
+["23-05"]="Eveline",["23-08"]="Jean Paul",["24-07"]="Arthur",["09-07"]="Jade",["27-03"]="Judith",["06-03"]="Annie",["02-11"]="Nicole"};
+
+
