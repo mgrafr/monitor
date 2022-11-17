a5c4c6e8fb0f3fdad3ff9b2254eb158648da5aab
domo-site <93765654+mgrafr@users.noreply.github.com>
2022-11-17 19:02:32 +0100

    modif spa

--- include/spa.php
+++ include/spa.php
@@ -3,8 +3,8 @@
 		<div id="spa" class="spa">
 			<div class="container">
 		<div class="col-md-12">
-	  <h1 class="title_ext text-center">SPA<span>terrasse</span></h1>
-		<p class="txt_ext">texte</p>	
+	  <h1 class="title_ext text-center">SPA<span> terrasse</span></h1>
+		
 			
 		   <?php //include ("spa_svg.php");?>
 </div>
