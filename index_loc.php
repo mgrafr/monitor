8176ef91106badaeef917ae53923664231486f74
domo-site <93765654+mgrafr@users.noreply.github.com>
2022-11-17 18:32:34 +0100

    ajout page

--- index_loc.php
+++ index_loc.php
@@ -37,6 +37,7 @@ if (ON_MUR==true) {include ("include/mur_cam.php");$_SESSION["zmuser"]=ZMUSER;
 $_SESSION["zmpass"]=ZMPASS;}
 if (ON_DVR==true) include ("include/dvr.php");
 if (ON_NAGIOS==true) include ("include/nagios.php");//monitoring
+if (ON_SPA==true) include ("include/spa.php");//monitoring
 include ("include/test_pass.php");// verif du mot de passe
 include ("include/footer.php");// fin de la page avec les scrpits JS
 ?>
