c2e6bd64d9926caed2e046de827a40cac612db71
domo-site <93765654+mgrafr@users.noreply.github.com>
2022-11-17 18:33:52 +0100

    ajout page spa

--- admin/config.php
+++ admin/config.php
@@ -1,11 +1,12 @@
 <?php
 // NE PAS MODIFIER LES VALEURS EN MAJUSCULES------
 //general monitor
-define('URLMONITOR', 'monitor.xxxxxxx.ovh');//domaine
+define('URLMONITOR', 'monitor.la-truffiere.ovh');//domaine
 define('IPMONITOR', '192.168.1.7');//ip 
 define('MONCONFIG', 'admin/config.php');//fichier config 
 define('DZCONFIG', 'admin/dz/temp.lua');//fichier temp 
 define('FAVICON', '/favicon.ico');//fichier favicon  , icone du domaine dans barre url
+define('DISPOSITIFS', 'dispositifs');
 // répertoire des images
 $rep='images/';//ne pas changer
 // images logo et titres
@@ -13,18 +14,18 @@ define('IMAGEACCUEIL', $rep.'maison.jpg');//image page accueil pour écrans >534
 define('IMAGEACCUEILSMALL', $rep.'maison_small.jpg');//image page accueil pour écrans <535 px
 define('IMGLOGO', $rep.'logo.png');//image logo
 define('NOMSITE', 'Domoticz');//nom principal du site
-define('NOMSLOGAN', 'Bonjour');//nom secondaire ou slogan
+define('NOMSLOGAN', 'La Truffière');//nom secondaire ou slogan
 // affichage lexique
-define('LEXIQUE', false);
+define('LEXIQUE', true);
 // infos de découverte , à mettre à FALSE en production
 define('DECOUVERTE', true);
 //
 define('TEMPSMAJSERVICES', 1800000);//interval maj services en milli secondes
 define('TEMPSMAJSERVICESAL', 180000);//interval maj services ALARME ABSENCE en milli secondes
 // caméras et VTO DAHUA
-define('DHPASSVTO','xxxxxxxx');
+define('DHPASSVTO','a1234567');
 define('DHUSER','michel');
-define('DHPASS','xxxxxxx');
+define('DHPASS','Idem4546');
 // --------------------------------------------------------------------------------------------
 // choix ID pour l'affichage des infos des dispositifs
 // idx : idx de Domoticz    idm : idm de monitor (dans ce cas la table "dispositifs" 
@@ -34,19 +35,19 @@ define('CHOIXID','idm');// idm ou idx
 define('NUMPLAN','2');// n° du plan regroupant tous les capteurs
 // parametres serveur DBMaria
 define('SERVEUR','localhost');
-define('MOTDEPASSE','00000000');
+define('MOTDEPASSE','Idem4546');
 define('UTILISATEUR','michel');
 define('DBASE','domoticz');
 //------Page  Alarmee & Administration------------
 // page Alarme
-define('ON_ALARM',false);// affichage pour utilisation de l'alarme
+define('ON_ALARM',true);// affichage pour utilisation de l'alarme
 // mot passe alarme et administation , la page administration est ON
-define('PWDALARM','000000');//mot passe alarme
+define('PWDALARM','004545');//mot passe alarme
 define('NOM_PASS_AL','pwdalarm');// nom du mot de passe dans la BD
 define('TIME_PASS_AL','3600');// temps de validité du mot de passe
 // ------------------------------------------------------------------------------------------
 //------Page  commandes Mur Inter------------------------------------
-define('ON_ONOFF',false);// affichage pour utilisation des commandes
+define('ON_ONOFF',true);// affichage pour utilisation des commandes
 // mot passe commande de dispositifs sensibles
 define('PWDCOMMAND','');//mot passe alarme
 define('NOM_PASS_CM','pwdcommand');// nom du mot de passe dans la BD
@@ -60,8 +61,8 @@ define('PILES', array( //id var domoticz, nom var domoticz, %1 (moyen), %2 (faib
 ));
 //---------------------------------------
 // Domoticz
-define('IPDOMOTICZ', '192.168.1.21');//ip
-define('URLDOMOTICZ', 'http://192.168.1.21:8082/');//url
+define('IPDOMOTICZ', '192.168.1.76');//ip
+define('URLDOMOTICZ', 'http://192.168.1.76:8086/');//url
 define('VARTAB', URLDOMOTICZ.'modules_lua/string_tableaux.lua');//url
 //-----------------------------------------------------------
 // Sauvegardes domoticz
@@ -71,49 +72,52 @@ define('FICVARDZ','var_dz');//fichier json sauvegarde des variables
 //**********************************************************
 // AFFICHAGE DE PAGES Pré installées
 // Page Météo  meteo concept
-define('ON_MET',false);// affichage page TOKEN PBLIGATOIRE
+define('ON_MET',true);// affichage page TOKEN PBLIGATOIRE
 // ---Token
-define('TOKEN','YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY');
+define('TOKEN','2fce16877b45b86ba110ef2cdbf8d0e437563395f7a8ab2961919a7065ea2cd0&insee=24454');
 //-----------------------------------------------------------------------------------------------
 // Page App diverses , log dz , nagios , sql
-define('ON_APP',false);// mise en service page extérieur
+define('ON_APP',true);// mise en service page extérieur
 // Page exterieur jardin plan extérieur
-define('ON_EXT',false);// mise en service page extérieur
+define('ON_EXT',true);// mise en service page extérieur
 // Page graphiques
-define('ON_GRAPH',false);// mise en service page graphique
+define('ON_GRAPH',true);// mise en service page graphique
 // Page MUR de Caméras-------------------------------------------
 // utilisation du mur :true sinon false , Nom du mur , nb caméras
-define('ON_MUR',false);// mise en service MUR
-define('NOMMUR','Mon Mur');// nom du mur
-define('NBCAM','1');// nombre caméras
+define('ON_MUR',true);// mise en service MUR
+define('NOMMUR','La Truffiere');// nom du mur
+define('NBCAM','6');// nombre caméras
 // Zoneminder
 define('ZMURL','http://192.168.1.9/zm');//IP/zm
 define('ZMURLTLS','https:zoneminder.la-truffiere.ovh');// sous domaine
-define('ZMUSER','moi');// pour mur_cameras.php
-define('ZMPASS','000000');// pour mur_cameras.php
+define('ZMUSER','michel');// pour mur_cameras.php
+define('ZMPASS','Idem4546');// pour mur_cameras.php
 define('TIMEAPI','3400');//suivant la valeur indiquée dans zoneminder
 //---------------------------------------------------------------------
 // Page zigbee2mqtt
-define('ON_ZIGBEE',false);// mise en service Zigbee
-define('IPZIGBEE', 'http://192.168.1.76:8088');//ip:port
-define('URLZIGBEE', 'https://zigbee.zzzzzzzzz.ovh');//url
+define('ON_ZIGBEE',true);// mise en service Zigbee
+define('IPZIGBEE', 'http://192.168.1.76:8080');//ip:port
+define('URLZIGBEE', 'https://zigbee.la-truffiere.ovh');//url
 //Page zwavejs2mqtt
-define('ON_ZWAVE',true);// mise en service agent DVR
+define('ON_ZWAVE',true);// mise en service Zwave
 define('IPZWAVE', 'http://192.168.1.76:8091');
-define('URLZWAVE', 'https://zwave.xxxxxxxxxxx.ovh');//url');
+define('URLZWAVE', 'https://zwave.la-truffiere.ovh');//url');
 // Page Monitoring
 //Nagios
-define('ON_NAGIOS',false);// mise en service Monitoring
+define('ON_NAGIOS',true);// mise en service Monitoring
 define('IPNAGIOS', '192.168.1.8/nagios');//ip/dossier
-define('URLNAGIOS', 'https://monitoring.zzzzzzzzz.ovh/nagios/');
+define('URLNAGIOS', 'https://monitoring.la-truffiere.ovh/nagios/');
 define('NAUSER', 'nagiosadmin');
-define('NAPASS', '00000000');
+define('NAPASS', 'Idem4546');
 //Page Mur de Caméras avec Agent DVR
 //Agent DVR
 define('ON_DVR',false);// mise en service agent DVR
 define('IPDVR', 'http://192.168.1.50:8090');
 define('URLDVR', 'https://DOMAINE.ovh');
+//SPA
+define('ON_SPA',false);// mise en service SPA
 //----------------------------------------------------------------
+
 ?>
 
 
