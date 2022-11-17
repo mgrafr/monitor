c07f496ce1d3f91012ada962498dd770d75b79ec
domo-site <93765654+mgrafr@users.noreply.github.com>
2022-11-17 18:35:03 +0100

    ajout page spa

---
+++ include/spa.php
@@ -0,0 +1,18 @@
+<!-- section SPA start -->
+<!-- ================ -->
+		<div id="spa" class="spa">
+			<div class="container">
+		<div class="col-md-12">
+	  <h1 class="title_ext text-center">SPA<span>terrasse</span></h1>
+		<p class="txt_ext">texte</p>	
+			
+		   <?php //include ("spa_svg.php");?>
+</div>
+</div>
+
+  </div>
+
+		
+
+			
+
