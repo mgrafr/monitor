7225ae0f04dc3f4373cd61e2b3299263d9995eaf
domo-site <93765654+mgrafr@users.noreply.github.com>
2022-11-20 10:01:08 +0100

    maj

--- scripts_dz/lua/export_sql.lua
+++ scripts_dz/lua/export_sql.lua
@@ -1,21 +1,9 @@
---notification à 19h30 et 20h30 , rappel possible à 20h"30 :
--- variable nb_not_tv = 2
 --
---
-commandArray = {}
-local time = string.sub(os.date("%X"), 1, 5)
-
---
---
-local idx="7";-- idx de la variable not_tv_ok
-function notification()
-		os.execute("echo 'Idem4546' |sudo -S node /home/michel/notification_lg.js "..texte.." "..idx.." not_tv_ok 2 1  >> /home/michel/poubelle.log 2>&1");
-        print(time.."..  maj notification");
-end
 --[[
 ****************]]
 --
-commandArray = {}
+
+--xxx="michel"; os.execute("python3 scripts/python/pushover.py "..xxx.." > /home/michel/fab.log 2>&1");
 year 	= tonumber(os.date("%Y"));
 month 	= os.date("%m");
 day 	= os.date("%d");
@@ -28,37 +16,43 @@ datetime = year.."-"..month.."-"..day.." "..time;
 
 function envoi_fab(don)
 	print ("maj temp:"..don);
-        local command = "/home/michel/domoticz/scripts/./fabric.sh"..don.." > /home/michel/fab.log 2>&1";
+        local command = "/bin/bash userdata/scripts/bash/./fabric.sh"..don.." > /home/michel/fab.log 2>&1";
         os.execute(command);
+        --txt="michel";os.execute("python3 scripts/python/pushover.py "..txt.." >> /home/michel/push.log 2>&1");
 end
 function round(num,numDecimal)
    local mult = 10^(numDecimal or 0)
   return math.floor(num * mult + 0.5) / mult
  end
+commandArray = {}
 
 for deviceName,deviceValue in pairs(devicechanged) do
     if (deviceName=='temp pir salon') then 
-	    libelle="salon";don=" "..libelle.."#"..tostring(deviceValue).."#"..datetime
+	    libelle="temp_salon";don=" "..libelle.."#"..tostring(deviceValue).."#"..datetime
         envoi_fab(don)
+    elseif (deviceName=='PH_Spa') then
+        local ph=round(deviceValue, 1)
+	    libelle="ph_spa";don=" "..libelle.."#"..ph.."#"..datetime
+	    envoi_fab(don)    
     elseif (deviceName=='temp pir ar cuisine') then 
-	    libelle="cuisine";don=" "..libelle.."#"..tostring(deviceValue).."#"..datetime
+	    libelle="temp_cuisine";don=" "..libelle.."#"..tostring(deviceValue).."#"..datetime
         envoi_fab(don)
     elseif (deviceName=='THB_Temperature') then 
         -- choix nb decimales apres la virgule = X (deviceValue, X)
-        -- local temp=round(deviceValue, 1)
+         --local temp=round(deviceValue, 1)
         local temp=round(deviceValue, 0)
         if tostring(temp)~=uservariables['temp_meteo'] then
         commandArray['Variable:temp_meteo'] = tostring(temp)    
-    	    libelle="meteo";don=" "..libelle.."#"..temp.."#"..datetime
+    	    libelle="temp_meteo";don=" "..libelle.."#"..temp.."#"..datetime
             envoi_fab(don)
         end
-     elseif (deviceName=='temp-hum_cave_Temperature') then 
+     elseif (deviceName=='temp_cave_Temperature') then 
         -- choix nb decimales apres la virgule = X (deviceValue, X)
-        -- local temp=round(deviceValue, 1)
-        local temp=round(deviceValue, 0)
+         local temp=round(deviceValue, 1)
+        --local temp=round(deviceValue, 0)
         if tostring(temp)~=uservariables['temp_cave'] then
         commandArray['Variable:temp_cave'] = tostring(temp)    
-    	    libelle="cave";don=" "..libelle.."#"..temp.."#"..datetime
+    	    libelle="temp_cave";don=" "..libelle.."#"..temp.."#"..datetime
             envoi_fab(don)
         end  
     elseif (deviceName=='temp_cellier_Temperature') then 
@@ -67,16 +61,16 @@ for deviceName,deviceValue in pairs(devicechanged) do
         local temp=round(deviceValue, 0)
         if tostring(temp)~=uservariables['temp_cellier'] then
         commandArray['Variable:temp_cellier'] = tostring(temp)    
-    	    libelle="cellier";don=" "..libelle.."#"..temp.."#"..datetime
+    	    libelle="temp_cellier";don=" "..libelle.."#"..temp.."#"..datetime
             envoi_fab(don)
         end   
-    elseif (deviceName=='temp_cuis_ete') then 
+    elseif (deviceName=='temp_cuisine_ete_Temperature') then 
         -- choix nb decimales apres la virgule = X (deviceValue, X)
         -- local temp=round(deviceValue, 1)
         local temp=round(deviceValue, 0)
         if tostring(temp)~=uservariables['temp_cuis_ete'] then
         commandArray['Variable:temp_cuis_ete'] = tostring(temp)    
-    	    libelle="cuis_ete";don=" "..libelle.."#"..temp.."#"..datetime
+    	    libelle="temp_cuis_ete";don=" "..libelle.."#"..temp.."#"..datetime
             envoi_fab(don)
         end       
     end
