5f6e351c31b6775df3a26879b8e3aeafe8b120e5
domo-site <93765654+mgrafr@users.noreply.github.com>
2022-12-14 17:53:50 +0100

    maj

--- include/lexique.php
+++ include/lexique.php
@@ -4,16 +4,17 @@
       <div class="modal-header">
         <h3 class="modal-title">Infos</h3>
 		 </div>
-			 <p style="margin-left:10px">Larg/Haut de l'écran :<span id="largeur"></span>/<span id="hauteur"></span></p>
-			 <p style="margin-left:10px"><img class="info1" src="images/poubelle_grise.svg" style="width:40px;height:auto;" >poubelle ménagère
+			 <p class="lex_txt">Larg/Haut de l'écran :<span id="largeur"></span>/<span id="hauteur"></span></p>
+			 <p class="lex_txt"><img class="info1" src="images/poubelle_grise.svg" style="width:40px;height:auto;" >poubelle ménagère
 			 <img class="info1" src="images/poubelle_grise_v.svg" style="width:40px;height:auto;" ></p>
-			 <p style="margin-left:10px"><img class="info1" src="images/poubelle_jaune.svg" style="width:40px;height:auto;" >poubelle recyclabes
+			 <p class="lex_txt"><img class="info1" src="images/poubelle_jaune.svg" style="width:40px;height:auto;" >poubelle recyclabes
 			 <img class="info1" src="images/poubelle_j_videe.svg" style="width:40px;height:auto;" ></p>
-			 <p style="margin-left:10px"><img class="info1" src="images/fosse_septique.svg" style="width:40px;height:auto;" >fosse septique à traiter</p>
-			 <p style="margin-left:10px"><img class="info1" src="images/pluie.svg" style="width:40px;height:auto;" >averse pluie imminente</p>
-			 <p style="margin-left:10px"><img class="info1" src="images/anni.svg" style="width:40px;height:auto;" >anniversaires<img class="info1" src="images/alarme_enable.svg" style="width:40px;height:auto;" >Alarme Nuit</p>
-			 <p style="margin-left:10px"><img class="info1" src="images/batterie_faible.svg" style="width:30px;height:auto;" ><img class="info1" src="images/batterie_moy.svg" style="width:30px;height:auto;" >niveau bas des piles</p>
-			 <p style="margin-left:10px"><img class="info1" src="images/ping_pi4.svg" style="width:40px;height:auto;" >PI4 hors service</p>
+			 <p class="lex_txt"><img class="info1" src="images/fosse_septique.svg" style="width:40px;height:auto;" >fosse septique à traiter</p>
+			 <p class="lex_txt"><img class="info1" src="images/pluie.svg" style="width:40px;height:auto;" >averse pluie imminente</p>
+			 <p class="lex_txt"><img class="info1" src="images/anni.svg" style="width:40px;height:auto;" >anniversaires</p>
+			 <p class="lex_txt"><img class="info1" src="images/batterie_faible.svg" style="width:30px;height:auto;" ><img class="info1" src="images/batterie_moy.svg" style="width:30px;height:auto;" >niveau bas des piles</p>
+			 <p class="lex_txt"><img class="info1" src="images/ping_pi4.svg" style="width:40px;height:auto;" >PI4 hors service</p>
+			<p class="lex_txt"><img class="info1" src="images/alarme_enable.svg" style="width:40px;height:auto;" >Alarme Nuit<img class="info1" src="images/alarme_auto.svg" style="width:40px;height:auto;" >alarme auto</p>
 			 
 			 			 </div>
      
