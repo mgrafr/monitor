a717de5c82ae83edc359cc47ca27e41505a43582
domo-site <93765654+mgrafr@users.noreply.github.com>
2022-12-18 13:42:44 +0100

    maj

--- scripts_dz/lua/notification_variables.lua
+++ scripts_dz/lua/notification_variables.lua
@@ -1,12 +1,12 @@
--- script notifications_autres
+-- script notifications_variables
 function alerte_gsm(txt)
-f = io.open("/home/michel/domoticz/scripts/python/aldz.py", "w")
-env="#!/usr/bin/env python3.7"
+f = io.open("userdata/scripts/python/aldz.py", "w")
+env="#!/usr/bin/env python3"
 f:write(env.." -*- coding: utf-8 -*-\nx='"..txt.."'")
 f:close()
 end
 -- repertoire du script python
-rep='/home/michel/domoticz/scripts/python/'
+rep='userdata/scripts/python/'
 -- repertoire log
 rep_log='/home/michel/'
 --
@@ -17,10 +17,19 @@ return {
 		    'boite_lettres',
 		    'upload',
 		    'zm_cam',
+		    'pression-chaudiere',
+		    'porte-ouverte',
+		    'intrusion'
 		}
 	},
 	execute = function(domoticz, variable)
-	    domoticz.log('Variable ' .. variable.name .. ' was changed', domoticz.LOG_INFO)
+	    --domoticz.log('Variable ' .. variable.name .. ' was changed', domoticz.LOG_INFO)
+	            if ((domoticz.variables('pression-chaudiere').changed) and (domoticz.variables('pression-chaudiere').value == "manque_pression")) then  
+	             txt=tostring(domoticz.variables('pression-chaudiere').value) 
+	             domoticz.variables('pression-chaudiere').set('pression_basse')
+	 	         print("envoi SMS pression-chaudiere")
+                 alerte_gsm('alarme_'..txt)
+               end
 	        
 	           if ((domoticz.variables('zm_cam').changed) and (domoticz.variables('zm_cam').value ~= "0")) then  
 	             txt=tostring(domoticz.variables('zm_cam').value) 
@@ -40,10 +49,10 @@ return {
             if (domoticz.variables('boite_lettres').changed) then
                 if (domoticz.variables('boite_lettres').value == "0") then 
                 print("topic envoyé : esp/in/boite_lettres")
-                 local command = "/home/michel/domoticz/scripts/python/mqtt.py esp/in/boite_lettres valeur 0   >> "..rep_log.."esp.log 2>&1" ;
+                 local command = "scripts/python/mqtt.py esp/in/boite_lettres valeur 0   >> "..rep_log.."esp.log 2>&1" ;
                  os.execute(command);    
                 end 
-           end 
+            end 
             if ((domoticz.variables('upload').changed) and (domoticz.variables('upload').value ~= "0")) then
                 if (domoticz.variables('upload').value == "1") then 
                 print("upload string_tableaux");
@@ -55,9 +64,18 @@ return {
                 --print(command);
                os.execute(command);print('maj effectuée');
                domoticz.variables('upload').set('0')   
-               
-           end 
-           
+            end 
+            if (domoticz.variables('porte-ouverte').changed) then  
+	             txt=tostring(domoticz.variables('porte-ouverte').value) 
+	             print("porte-ouverte")
+                 alerte_gsm('alarme_'..txt)
+            end
+            if (domoticz.variables('intrusion').changed) then  
+	             txt=tostring(domoticz.variables('intrusion').value) 
+	             print('intrusion')
+                 alerte_gsm('alarme_'..txt)
+            end
     end
+   
 }
 
