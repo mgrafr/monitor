8aeed41b75f22c381212d011025e07b874cf8609
domo-site <93765654+mgrafr@users.noreply.github.com>
2022-12-22 11:34:21 +0100

    maj

--- scripts_dz/lua/export_sql.lua
+++ scripts_dz/lua/export_sql.lua
@@ -32,45 +32,50 @@ function round(num,numDecimal)
  end
 commandArray = {}
 t = {};
-
+-- libelle=table#champ
+-- si 2 champs , ajouter ..'#'..libelle2 après datetime 
+-- exemple "don=" "..libelle.."#"..tostring(deviceValue).."#"..datetime..'#'..libelle2
 for deviceName,deviceValue in pairs(devicechanged) do
-    if (deviceName=='pir salon') then
+    if (deviceName=='temp_pir_salon_temperature_air') then
         print ("temp_salon:"..deviceValue);
-	    libelle="temp_salon";don=" "..libelle.."#"..tostring(deviceValue).."#"..datetime
+	    libelle="temp_salon#valeur";don=" "..libelle.."#"..tostring(deviceValue).."#"..datetime
         envoi_fab(don)
     elseif (deviceName=='pression_chaudière') then 
         pressionch=tonumber(deviceValue);
         print ("pression_chaudiere:"..pressionch.."--"..pression);
         if (pression~=pressionch) then 
-            libelle="pression_chaudiere";don=" "..libelle.."#"..tostring(deviceValue).."#"..datetime
+            libelle="pression_chaudiere#valeur";don=" "..libelle.."#"..tostring(deviceValue).."#"..datetime
             envoi_fab(don) 
             write_datas(tonumber(deviceValue))
             if (pression<1) then commandArray['Variable:pression-chaudiere'] = "manque_pression";  print("pression basse")
             else commandArray['Variable:pression-chaudiere']="ras"    
             end
         end
-        
+     elseif (deviceName=='truffiere - Linky 16267727923561') then
+        --local mesure=round(deviceValue, 1)
+	    libelle="pmax#valeur";don=" "..libelle.."#"..mesure.."#"..datetime
+	    envoi_fab(don)    
     elseif (deviceName=='PH_Spa') then
         local mesure=round(deviceValue, 1)
-	    libelle="ph_spa";don=" "..libelle.."#"..mesure.."#"..datetime
+	    libelle="ph_spa#valeur";don=" "..libelle.."#"..mesure.."#"..datetime
 	    envoi_fab(don)
 	 elseif (deviceName=='Redox_Spa') then
         local mesure=round(deviceValue, 1)
-	    libelle="orp_spa";don=" "..libelle.."#"..mesure.."#"..datetime
+	    libelle="orp_spa#valeur";don=" "..libelle.."#"..mesure.."#"..datetime
 	    envoi_fab(don)
 	 elseif (deviceName=='Temp-eau_SPA') then
         local mesure=round(deviceValue, 1)
-	    libelle="temp_spa";don=" "..libelle.."#"..mesure.."#"..datetime
+	    libelle="temp_spa#valeur";don=" "..libelle.."#"..mesure.."#"..datetime
 	    envoi_fab(don)   
 	 elseif (deviceName=='Debit_filtration_SPA') then
 	    print ("debit:"..deviceValue);c=0;
 	    for i in string.gmatch(deviceValue,"[^;]+") do
         t[c]=i;c=c+1;
         end
-	    libelle="debit_spa";don=" "..libelle.."#"..t[0].."#"..datetime
+	    libelle="debit_spa#valeur";don=" "..libelle.."#"..t[0].."#"..datetime
 	    envoi_fab(don)   
     elseif (deviceName=='temp pir ar cuisine') then 
-	    libelle="temp_cuisine";don=" "..libelle.."#"..tostring(deviceValue).."#"..datetime
+	    libelle="temp_cuisine#valeur";don=" "..libelle.."#"..tostring(deviceValue).."#"..datetime
         envoi_fab(don)
     elseif (deviceName=='THB_Temperature') then 
         -- choix nb decimales apres la virgule = X (deviceValue, X)
@@ -78,7 +83,7 @@ for deviceName,deviceValue in pairs(devicechanged) do
         local temp=round(deviceValue, 0)
         if tostring(temp)~=uservariables['temp_meteo'] then
         commandArray['Variable:temp_meteo'] = tostring(temp)    
-    	    libelle="temp_meteo";don=" "..libelle.."#"..temp.."#"..datetime
+    	    libelle="temp_meteo#valeur";don=" "..libelle.."#"..temp.."#"..datetime
             envoi_fab(don)
         end
      elseif (deviceName=='temp_cave_Temperature') then 
@@ -87,7 +92,7 @@ for deviceName,deviceValue in pairs(devicechanged) do
         --local temp=round(deviceValue, 0)
         if tostring(temp)~=uservariables['temp_cave'] then
         commandArray['Variable:temp_cave'] = tostring(temp)    
-    	    libelle="temp_cave";don=" "..libelle.."#"..temp.."#"..datetime
+    	    libelle="temp_cave#valeur";don=" "..libelle.."#"..temp.."#"..datetime
             envoi_fab(don)
         end  
     elseif (deviceName=='temp_cellier_Temperature') then 
@@ -96,7 +101,7 @@ for deviceName,deviceValue in pairs(devicechanged) do
         local temp=round(deviceValue, 0)
         if tostring(temp)~=uservariables['temp_cellier'] then
         commandArray['Variable:temp_cellier'] = tostring(temp)    
-    	    libelle="temp_cellier";don=" "..libelle.."#"..temp.."#"..datetime
+    	    libelle="temp_cellier#valeur";don=" "..libelle.."#"..temp.."#"..datetime
             envoi_fab(don)
         end   
     elseif (deviceName=='temp_cuisine_ete_Temperature') then 
@@ -105,7 +110,7 @@ for deviceName,deviceValue in pairs(devicechanged) do
         local temp=round(deviceValue, 0)
         if tostring(temp)~=uservariables['temp_cuis_ete'] then
         commandArray['Variable:temp_cuis_ete'] = tostring(temp)    
-    	    libelle="temp_cuis_ete";don=" "..libelle.."#"..temp.."#"..datetime
+    	    libelle="temp_cuis_ete#valeur";don=" "..libelle.."#"..temp.."#"..datetime
             envoi_fab(don)
         end       
     end
