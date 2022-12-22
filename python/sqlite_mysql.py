4dfaa9f32362fcf062132ff53adf9f0fcfba2d15
domo-site <93765654+mgrafr@users.noreply.github.com>
2022-12-22 11:35:54 +0100

    maj

--- python/sqlite_mysql.py
+++ python/sqlite_mysql.py
@@ -4,20 +4,21 @@ import sys
 import mysql.connector
 from mysql.connector import Error
 total_arg = len(sys.argv)
-if (total_arg>0) : 
+if (total_arg>0) :
     x= str(sys.argv[1])
-    temp = x.split('#') 
+    temp = x.split('#')
     table=temp[0]
-    val1=temp[1]
-    val=temp[2]+" "+temp[3]
-    print(table)
-    print(val)
-    print(val1)               
+    champ=temp[1]
+    val1=temp[2]
+    val=temp[3]+" "+temp[4]
+if (len(x)==7) :
+    champ2=temp[5]
+    val2=temp[6]
 try:
     connection = mysql.connector.connect(
-          host = "127.0.0.1", 
-          user = "michel", 
-          password = "Idem4546", 
+          host = "127.0.0.1",
+          user = "michel",
+          password = "Idem4546",
           database = "domoticz")
 
     if connection.is_connected():
@@ -27,21 +28,24 @@ try:
         cursor.execute("select database();")
         record = cursor.fetchone()
         print("You're connected to database: ", record)
-        query = "INSERT INTO "+table+" (date,valeur) VALUES(%s, %s)"
-        values = (val, val1)
+        if (len(x)==7) :
+            query = "INSERT INTO "+table+" (date,"+champ+","+champ2+") VALUES(%>
+            values = (val, val1, val2)
+        else :
+            query = "INSERT INTO "+table+" (date,"+champ+") VALUES(%s, %s)"
+            values = (val, val1)
         cursor.execute(query, values)
-                          
-        
+
+
     connection.commit()
     print(cursor.rowcount, "Record inserted successfully into Laptop table")
-        
-        
-        
+
+
+
 
 except Error as e:
     print("Error while connecting to MySQL", e)
 finally:
     if (connection.is_connected()):
         cursor.close()
-        connection.close()
-        print("MySQL connection is closed")
+
