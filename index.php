b57f3c9f2bda40253ea2b40957fd77a1d22bd025
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-01-07 16:48:55 +0100

    maj

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php
 echo '<!DOCTYPE html><html><body style="background-color: cornsilk;">';
 
-$rep="/"; $domaine=$_SERVER['HTTP_HOST'];
-if (substr($domaine, 0, -2)=="192.168.1" || substr($domaine, 0, -2)=="192.168.1.") $rep="/monitor/";
+$rep="/"; $domaine=$_SERVER['HTTP_HOST'];$port=$_SERVER['SERVER_PORT'];
+
+if (substr($domaine, 0, 7)=="192.168") $rep="/monitor/";
 echo '
 <script>
 var larg = screen.width;
 if (larg<769 ){ window.location.href="'.$rep.'index_loc.php";}
+else function loadPage(url){
+				document.getElementById("inline_monitor").contentWindow.document.location.href=url;
+			}
 </script>';
 echo '
 <iframe id="inline_monitor"
