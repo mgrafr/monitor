1300bc379fc4052db4b92c9429de38754ee3ed67
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-01-12 17:53:07 +0100

    maj

--- include/zwave.php
+++ include/zwave.php
@@ -1,20 +1,21 @@
 <?php
 session_start();
-$domaine=$_SESSION["domaine"];
+$domaine=$_SESSION["domaine"];echo "AZERTY:".$domaine;
 if ($domaine==URLMONITOR) $lien_zwave=URLZWAVE;
 if ($domaine==IPMONITOR) $lien_zwave=IPZWAVE;
-?>
+
+echo'
 <!-- section zwavejs2mqtt start -->
 <!-- ================ -->
 <div id="zwave" >
 	<div class="container">
 		<div class="col-md-12">
 	  <h1 id="about" class="title" style="position:relative;top:-30px;">Dispositifs : <span style="color:blue">Zwave</span></h1>
-		         <iframe id="zwmqtt" src="<?php echo $lien_zwave;?>" frameborder="0"></iframe>
+		         <iframe id="zwmqtt" src="'.$lien_zwave.'" frameborder="0"></iframe>
 		<div class="modal" id="infos"></div>
 </div>
 	</div>
 		</div> 		
-<!-- section zigbee fin-->
+<!-- section zigbee fin-->';
 
 
