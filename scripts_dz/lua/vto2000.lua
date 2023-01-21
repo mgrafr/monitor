a22e02c58cb1a78f7e5f4a134d578330ea7322f9
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-01-21 18:29:32 +0100

    Add files via upload

--- scripts_dz/lua/vto2000.lua
+++ scripts_dz/lua/vto2000.lua
@@ -6,10 +6,8 @@ commandArray = {}
 --
 -- 
 if (uservariables['sonnette']=="1") then 
---          envoi sms 
-            os.execute("curl --insecure  'https://smsapi.free-mobile.fr/sendmsg?user=LOGIN&pass=MOT_PASSE&msg=on sonne au portier' >> /home/michel/OsExecute1.log 2>&1")
---          envoi image pushover ---------------
-            os.execute("echo 'MOT_PASSE' |sudo -S /home/michel/domoticz/scripts/pushover_img.sh >> /home/michel/push.log 2>&1");
+--          --envoi image pushover ---------------
+            os.execute("/bin/bash userdata/scripts/bash/pushover_img.sh >> /home/michel/push.log 2>&1");
             commandArray['Variable:sonnette'] = '0'
 --        
 --    
