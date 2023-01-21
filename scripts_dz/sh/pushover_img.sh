83f579ebdaa5e3f5dca1739887ae59638f4ce4b0
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-01-21 11:12:38 +0100

    maj

--- scripts_dz/sh/pushover_img.sh
+++ scripts_dz/sh/pushover_img.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 jour=$(date +%H:%M:%S)
-wget http://admin:a1234567@192.168.1.111/cgi-bin/snapshot.cgi?0.jpg 
+wget http://LOGIN:PASS@192.168.1.111/cgi-bin/snapshot.cgi?0.jpg 
 mv snapshot.cgi?0.jpg image.jpg
 TITLE="image portier"
-APP_TOKEN="asa28r7g15o8o28mgvufyc7ny4rxka"
-USER_TOKEN="uoj2ks6quy86rpn51bmuv6ageau6ji"
+APP_TOKEN="xxxxxxxxxtokenxxxxxxxxx"
+USER_TOKEN="userxxxxxxxxxxxxxxxxx"
 MESSAGE="on sonne au portail" 
 curl -s -F "token=$APP_TOKEN" \
    -F "user=$USER_TOKEN" \
