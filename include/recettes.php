7cc820f7dbf461fba77fedf24b0cc55aa5a1c47a
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-01-27 10:36:07 +0100

    maj

--- include/recettes.php
+++ include/recettes.php
@@ -4,8 +4,8 @@
 	<div class="container">
 		<div class="col-md-12">
 	  <h1 id="about" class="title text-center">Recettes de <span> CUISINE</span></h1>
-		<div class="recette"><img class="btn_appd" rel="cuisine" src="images/pate_pizza.jpg" href="#recettes" data-toggle="modal" data-target="#recette" title="1" width="200" height="150" alt="pate à pizza"/>
-		<img class="btn_appd" rel="cuisine" src="images/croque_monsieur.jpg" href="#recettes" data-toggle="modal" data-target="#recette" title="2" width="200" height="150" alt="croque-monsieurs"/>
+		<div class="recette"><img class="btn_appd" rel="cuisine" src="images/pate_pizza.webp" href="#recettes" data-toggle="modal" data-target="#recette" title="1" width="200" height="150" alt="pate à pizza"/>
+		<img class="btn_appd" rel="cuisine" src="images/croque_monsieur.webp" href="#recettes" data-toggle="modal" data-target="#recette" title="2" width="200" height="150" alt="croque-monsieurs"/>
 			</div>
 </div>
 	</div>
