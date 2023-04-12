f1b9d3a69278a28c943e00b6ef738b75fc1f6d88
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-04-12 19:11:51 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -5,7 +5,9 @@ echo '<!DOCTYPE html><html><body style="background-color: cornsilk;">';
 $rep="/"; $domaine=$_SERVER['HTTP_HOST'];$port=$_SERVER['SERVER_PORT'];
 
 if (substr($domaine, 0, 7)=="192.168") $rep="/monitor/";
-echo '
+header('Location: '.$rep.'index_loc.php');
+  exit();
+/*echo '
 <script>
 var larg = screen.width;
 if (larg<769 ){ window.location.href="'.$rep.'index_loc.php";}
@@ -17,5 +19,5 @@ echo '
 <iframe id="inline_monitor"
     style="width:768px;height:1024px;margin:0 30%;background-color: cornsilk;"
     src="'.$rep.'index_loc.php">
-</iframe></body></html>';
+</iframe></body></html>';*/
 ?>
