b4407754c68093458ef9423dd07096ea04cc6322
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-04-16 15:41:15 +0200

    maj

--- share/scripts_dz/py/upload_fichier.py
+++ share/scripts_dz/py/upload_fichier.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import requests, sys
+from connect import ip_monitor
 x= str(sys.argv[1])
-ip= str(sys.argv[2])
+ip= ip_monitor
 rep="/opt/domoticz/www/modules_lua/"
 addr="http://"+ip+"/monitor/admin/dz/temp.lua"
 req = requests.get(addr)
