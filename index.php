2dfcd3a6e995ef28700ed7ec4bfe8d8cac0dd8db
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-04-16 10:00:44 +0200

    maj

--- index.php
+++ index.php
@@ -7,17 +7,5 @@ $rep="/"; $domaine=$_SERVER['HTTP_HOST'];$port=$_SERVER['SERVER_PORT'];
 if (substr($domaine, 0, 7)=="192.168") $rep="/monitor/";
 header('Location: '.$rep.'index_loc.php');
   exit();
-/*echo '
-<script>
-var larg = screen.width;
-if (larg<769 ){ window.location.href="'.$rep.'index_loc.php";}
-else function loadPage(url){
-				document.getElementById("inline_monitor").contentWindow.document.location.href=url;
-			}
-</script>';
-echo '
-<iframe id="inline_monitor"
-    style="width:768px;height:1024px;margin:0 30%;background-color: cornsilk;"
-    src="'.$rep.'index_loc.php">
-</iframe></body></html>';*/
+
 ?>
