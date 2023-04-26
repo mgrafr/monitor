8b02c8d4ea57c90a7311b23afff8d07d148671c0
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-04-26 18:42:12 +0200

    Add files via upload

---
+++ share/scripts_dz/py/connect.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3 -*- coding: utf-8 -*-
+login_free='MTI4MTI2MjA='
+pass_free='MkZRVE1NN3g0MmtzcHI='
+ip_monitor='192.168.1.9'
+ip_mqtt='192.168.1.42'
+ip_tv='192.168.1.24'
+ip_domoticz='192.168.1.76:8086'
