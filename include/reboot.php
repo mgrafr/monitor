82c1df03a9e4b18f8e8170e67abb1af591075d2f
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-05-02 18:58:06 +0200

    maj

--- include/reboot.php
+++ include/reboot.php
@@ -1,18 +1,14 @@
 <?php
-/* Notify the user if the server terminates the connection */
-return;
-
 
 $connection = ssh2_connect('192.168.1.8', 22);
 if (ssh2_auth_password($connection, 'michel', 'Idem4546')) {
   echo "Authentication Successful!\n";
-	
-			//demarrage de la base de donnée	
-			$stream = ssh2_exec($connection, 'bash "/var/www/html/./reboot.sh"  >> /home/michel/sms.log 2>&1');
-			
-} else {
-  die('Authentication Failed...');
-}
+  //envoi de la commande	
+  $stream = ssh2_exec($connection, 'bash "/var/www/html/./reboot.sh"  >> /home/michel/sms.log 2>&1');
+  //stream_set_blocking($stream, true);
+		//if($output = stream_get_contents($stream)){echo $output."<br>";}
+} 
+else {die('Authentication Failed...');}
 
 
 
