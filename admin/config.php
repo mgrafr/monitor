de8e061f56ac11cd793df6a2d634a93c9ac8c6fd
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-05-04 11:56:56 +0200

    Add files via upload

--- admin/config.php
+++ admin/config.php
@@ -1,8 +1,9 @@
 <?php
 // NE PAS MODIFIER LES VALEURS EN MAJUSCULES------
 //general monitor
-define('URLMONITOR', 'monitor.DOMAINE.ovh');//domaine si port autre 443 
-define('IPMONITOR', 'ip');//ip 
+define('URLMONITOR', 'monitor.la-truffiere.ovh');//domaine si port autre 443 
+#define('URLMONITOR', 'monitor.la-truffiere.ovh');//domaine
+define('IPMONITOR', '192.168.1.9');//ip 
 define('MONCONFIG', 'admin/config.php');//fichier config 
 define('DZCONFIG', 'admin/dz/temp.lua');//fichier temp 
 define('FAVICON', '/favicon.ico');//fichier favicon  , icone du domaine dans barre url
@@ -13,12 +14,12 @@ $rep='images/';//ne pas changer
 define('IMAGEACCUEIL', $rep.'maison.webp');//image page accueil pour écrans >534 px
 define('IMAGEACCUEILSMALL', $rep.'maison_small.webp');//image page accueil pour écrans <535 px
 define('IMGLOGO', $rep.'logo.webp');//image logo
-define('NOMSITE', 'nom du site');//nom principal du site
-define('NOMSLOGAN', 'nom secondaire');//nom secondaire ou slogan
+define('NOMSITE', 'Domoticz');//nom principal du site
+define('NOMSLOGAN', 'La Truffière');//nom secondaire ou slogan
 // affichage lexique
 define('LEXIQUE', true);
 // infos de découverte , à mettre à FALSE en production
-define('DECOUVERTE', true);
+define('DECOUVERTE', false);
 // serveur MQTT utilisation d'un serveur (envoi topic depuis monitor)
 define('MQTT', false);//  true si serveur MQTT utilisé par monitor
 define('MQTT_IP', '192.168.1.24');//adresse IP
@@ -31,14 +32,14 @@ define('TEMPSMAJSERVICESAL', 180000);//interval maj services ALARME ABSENCE en m
 define('TEMPO_DEVICES', 180000);// en milli secondes
 define('TEMPO_DEVICES_DZ', 30000);// en milli secondes (>= 15s) maj déclenchée par Dz voir doc
 // caméras et VTO DAHUA
-define('DHPASSVTO','xxxxxxxxxxxxxx');
+define('DHPASSVTO','a1234567');
 define('DHCAMPASS', array( //id var domoticz, nom var domoticz, %1 (moyen), %2 (faible) de l'energie restante  
-    '192.168.1.107' => 'MOT PASSE',
-    '192.168.1.108' => 'MOT PASSE',
-    '192.168.1.114' => 'MOT PASSE'
+    '192.168.1.107' => 'IdemIdem4546',
+    '192.168.1.108' => 'Idem4546',
+    '192.168.1.114' => 'Idem004546'
 ));
-define('DHUSER','USER');
-//define('DHPASS','MOT PASSE');
+define('DHUSER','michel');
+//define('DHPASS','Idem4546');
 // --------------------------------------------------------------------------------------------
 // choix ID pour l'affichage des infos des dispositifs
 // idx : idx de Domoticz    idm : idm de monitor (dans ce cas la table "dispositifs" 
@@ -48,21 +49,21 @@ define('CHOIXID','idm');// DZ:idm ou idx ; HA : idm uniquement
 define('NUMPLAN','2');//DZ uniquement: n° du plan regroupant tous les capteurs
 // parametres serveur DBMaria
 define('SERVEUR','localhost');
-define('MOTDEPASSE','PASS_BD');
-define('UTILISATEUR','USER_BD');
+define('MOTDEPASSE','Idem4546');
+define('UTILISATEUR','michel');
 define('DBASE','monitor');
 //------Page  Alarmee & Administration------------
 // page Alarme
 define('ON_ALARM',true);// affichage pour utilisation de l'alarme
 // mot passe alarme et administation , la page administration est ON
-define('PWDALARM','1234');//mot passe alarme
+define('PWDALARM','004546');//mot passe alarme
 define('NOM_PASS_AL','pwdalarm');// nom du mot de passe dans la BD
 define('TIME_PASS_AL','3600');// temps de validité du mot de passe
 // ------------------------------------------------------------------------------------------
 //------Page  commandes Mur Inter------------------------------------
 define('ON_ONOFF',true);// affichage pour utilisation des commandes
 // mot passe commande de dispositifs sensibles
-define('PWDCOMMAND','');//mot passe commandes
+define('PWDCOMMAND','');//mot passe alarme
 define('NOM_PASS_CM','pwdcommand');// nom du mot de passe dans la BD
 define('TIME_PASS_CM','43200');// temps de validité du mot de passe
 //-------------------Alarme piles-----------------------------
@@ -73,19 +74,20 @@ define('PILES', array( //id var domoticz, nom var domoticz, %1 (moyen), %2 (faib
 	20
 ));
 //---------------------------------------
-// Serveurs Domoticz ou HA
-define('IPDOMOTIC', 'ip');//ip 1er serveur Domotique
+// Domoticz ou HA
+define('IPDOMOTIC', '192.168.1.76');//ip
 define('URLDOMOTIC', 'http://192.168.1.76:8086/');//url
-define('DOMDOMOTIC', 'https://domoticz.DOMAINE.ovh');//domaine
-define('TOKENDOMOTIC', 'ENTRER LE TOKEN ICI');//TOKEN ou BEARER
-//.........................................
-define('IPDOMOTIC1', '192.168.1.5');//ip 2emme serveur Domotique
+define('DOMDOMOTIC', 'https://domoticz.la-truffiere.ovh');//domaine
+define('TOKENDOMOTIC1', '');//TOKEN ou BEARER
+define('IPDOMOTIC1', '');//ip 2emme serveur Domotique
 define('URLDOMOTIC1', 'http://192.168.1.5:8123/');//url ex:http://192.168.1.5:8123/
-define('DOMDOMOTIC1', 'https://ha.DOMAINE.ovh');//domaine
-define('TOKENDOMOTIC1', "ENTRER LE TOKEN ICI");//TOKEN ou BEARER
+define('DOMDOMOTIC1', 'https://ha.la-truffiere.ovh');//domaine
+define('TOKEN_DOM1',"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI3NzE0ZGY2ZDRlZmU0ZWQzYmI4MmI5YTRmN2NlM2UxYSIsImlhdCI6MTY3ODg4ODc1MSwiZXhwIjoxOTk0MjQ4NzUxfQ.UvvrBR60YRqHGqeYZV76nChlWoS1pW-evPJTP4Fcg2k");   
+
 //*************************Pour Domoticz
 define('VARTAB', URLDOMOTIC.'modules_lua/string_tableaux.lua');//
 define('BASE64', URLDOMOTIC.'modules_lua/connect.lua');//login et password en Base64
+define('CONF_MODECT', URLDOMOTIC.'modules_lua/string_modect.lua');
 //-----------------------------------------------------------
 // Sauvegardes domoticz
 define('FICVARDZ','var_dz');//fichier json sauvegarde des variables
@@ -96,8 +98,8 @@ define('FICVARDZ','var_dz');//fichier json sauvegarde des variables
 // Page Météo  meteo concept
 define('ON_MET',true);// affichage page TOKEN PBLIGATOIRE
 // ---Token & code insee
-define('TOKEN_MC','');
-define('INSEE','');
+define('TOKEN_MC','2fce16877b45b86ba110ef2cdbf8d0e437563395f7a8ab2961919a7065ea2cd0');
+define('INSEE','24454');
 // Alertes Pluie de  Météo France
 // Token
 define('TOKEN_MF','__Wj7dVSTjV9YGu1guveLyDq0g7S7TfTjaHBTPTpO0kj8__');
@@ -111,28 +113,28 @@ define('ON_GRAPH',true);// mise en service page graphique
 // Page MUR de Caméras-------------------------------------------
 // utilisation du mur :true sinon false , Nom du mur , nb caméras
 define('ON_MUR',true);// mise en service MUR
-define('NOMMUR','');// nom du mur
-define('NBCAM','0');// nombre caméras
+define('NOMMUR','La Truffiere');// nom du mur
+define('NBCAM','8');// nombre caméras
 // Zoneminder
 define('ZMURL','http://192.168.1.23/zm');//IP/zm
-define('ZMURLTLS','https:zoneminder.DOMAINE.ovh');// sous domaine
-define('ZMUSER','');// pour mur_cameras.php
-define('ZMPASS','MOT PASSE');// pour mur_cameras.php
+define('ZMURLTLS','https:zoneminder.la-truffiere.ovh');// sous domaine
+define('ZMUSER','michel');// pour mur_cameras.php
+define('ZMPASS','Idem4546');// pour mur_cameras.php
 define('TIMEAPI','3400');//suivant la valeur indiquée dans zoneminder
 //---------------------------------------------------------------------
 // Page zigbee2mqtt
 define('ON_ZIGBEE',true);// mise en service Zigbee
 define('IPZIGBEE', 'http://192.168.1.92:8084');//ip:port
-define('URLZIGBEE', 'https://zigbee.DOMAINE.ovh');//url
+define('URLZIGBEE', 'https://zigbee.la-truffiere.ovh');//url
 //Page zwavejs2mqtt
 define('ON_ZWAVE',true);// mise en service Zwave
 define('IPZWAVE', 'http://192.168.1.76:8091');
-define('URLZWAVE', 'https://zwave.DOMAINE.ovh');//url');
+define('URLZWAVE', 'https://zwave.la-truffiere.ovh');//url');
 // Page Monitoring
 //Nagios
 define('ON_NAGIOS',true);// mise en service Monitoring
 define('IPNAGIOS', '192.168.1.8/nagios');//ip/dossier
-define('URLNAGIOS', 'https://monitoring.DOMAINE.ovh/nagios/');
+define('URLNAGIOS', 'https://monitoring.la-truffiere.ovh/nagios/');
 define('NAUSER', 'nagiosadmin');
 define('NAPASS', 'Idem4546');
 //Page Mur de Caméras avec Agent DVR
@@ -153,5 +155,9 @@ define('ECRANSPA', array(
 // Recettes Cuisine
 define('ON_RECETTES',true);
 //----------------------------------------------------------------
-
+//fichiers divers
+define('MSMTPRC_LOC_PATH', '/var/www/html/monitor/scripts/');//copie config serveur mail
 ?>
+
+
+
