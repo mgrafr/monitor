6f058ff7373740b38631d092503e87382876fcb7
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-05-04 19:20:38 +0200

    maj

--- fonctions.php
+++ fonctions.php
@@ -801,8 +801,8 @@ if (($choix==10) || ($choix==11)) {$file = CONF_MODECT;$rel="11";}
 if (($choix==15) || ($choix==16)) {$file = BASE64;$rel="16";}	
 if (($choix==5) || ($choix==6)) {$file = MONCONFIG;$rel="6";}
 if (($choix==7) || ($choix==8)) {$file = MONCONFIG;$rel="8";}
-if ($choix==21 ) {$mode="scp_r";include ('include/ssh_scp.php');$file= MSMTPRC_LOC_PATH."msmtprc"; echo "copy de  msmtprc";$rel="22";}	
-//if ($choix==22 ) {$rel="22";//$file= MSMTPRC;
+if ($choix==21 ) {$mode="scp_r";include ('include/ssh_scp.php');$file= MSMTPRC_LOC_PATH."msmtprc"; echo "copy de  msmtprc";$rel="22";}
+if ($choix==22 ) {$file= MSMTPRC_LOC_PATH."msmtprc"; }	
 if (($choix!=4) && ($choix!=6) && ($choix!=8) && ($choix!=10) && ($choix!=11) && ($choix!=16) && ($choix!=22)) {echo '<p id="btclose"><img id="bouton_close" onclick="yajax('.$idrep.')"  
 src="images/bouton-fermer.svg" style="width:30px;height:30px;"/></p>';}	
 if ($choix==12 || $choix==13){echo "//*******création fichier noms/idx******* <br>";}
@@ -820,7 +820,7 @@ case "3" :
 case "5" :
 case "7" :
 case "15" :
-//case "21" :		
+case "21" :		
 echo $file.'<div id="result"><form >';
      $content = file_get_contents($file);
 	 if($choix==3){ file_put_contents(DZCONFIG.'.bak.'.$time, $content);}	 
@@ -834,7 +834,6 @@ return "sauvegarde OK";
 break;
 case "4" :
 case "16" :
-//case "22" :		
 $content=$idrep;
 echo '<p id="btclose"><img id="bouton_close" onclick="yajax(\'#reponse1\')" src="images/bouton-fermer.svg" style="width:30px;height:30px;"/></p>';		
 file_put_contents(DZCONFIG, $content);
@@ -844,8 +843,11 @@ if ($choix==16){$retour=maj_variable("22","upload","3","2");echo "Logins , mots 
 else {$retour['status'];}		
 break;
 case "6" :
+case "22":	
  $content=$idrep;
  file_put_contents($file, $content);
+if ($choix==22){$mode="scp_s";include ('include/ssh_scp.php');
+	echo '<p id="btclose"><img id="bouton_close" onclick="yajax(\'#reponse1\')" src="images/bouton-fermer.svg" style="width:30px;height:30px;"/>maj config msmtprc</p>';}		
  return;
  break;
 case "8" :
@@ -904,6 +906,10 @@ case "19" : $retour=sql_variable("",2) ;$n=0;
 break;
 case "20" :$mode="ssh";include ('include/ssh_scp.php'); echo "reboot Raspberry";
 return;	
+case "22" :
+		$mode="scp_s";include ('include/ssh_scp.php'); echo "copy de  msmtprc";
+return;	
+break;			
 break;
 case "23" :$mode="scp_s";include ('include/ssh_scp.php'); echo "copy de  msmtprc";
 return;	
