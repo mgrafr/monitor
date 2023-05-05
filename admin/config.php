b3359e3c473caf65e71a23a4e8b97b7a74062146
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-05-05 19:00:36 +0200

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -1,8 +1,8 @@
 <?php
 // NE PAS MODIFIER LES VALEURS EN MAJUSCULES------
 //general monitor
-define('URLMONITOR', 'monitor.la-truffiere.ovh');//domaine si port autre 443 
-#define('URLMONITOR', 'monitor.la-truffiere.ovh');//domaine
+define('URLMONITOR', 'monitor.xxxxxxxx');//domaine si port autre 443 
+#define('URLMONITOR', 'monitor.xxxxxxxx.ovh');//domaine
 define('IPMONITOR', '192.168.1.9');//ip 
 define('MONCONFIG', 'admin/config.php');//fichier config 
 define('DZCONFIG', 'admin/dz/temp.lua');//fichier temp 
@@ -34,12 +34,12 @@ define('TEMPO_DEVICES_DZ', 30000);// en milli secondes (>= 15s) maj déclenchée
 // caméras et VTO DAHUA
 define('DHPASSVTO','a1234567');
 define('DHCAMPASS', array( //id var domoticz, nom var domoticz, %1 (moyen), %2 (faible) de l'energie restante  
-    '192.168.1.107' => 'IdemIdem4546',
-    '192.168.1.108' => 'Idem4546',
-    '192.168.1.114' => 'Idem004546'
+    '192.168.1.107' => 'xxxx',
+    '192.168.1.108' => 'xxxxx',
+    '192.168.1.114' => 'xxxxxxxx6'
 ));
 define('DHUSER','michel');
-//define('DHPASS','Idem4546');
+//define('DHPASS','xxxxxx');
 // --------------------------------------------------------------------------------------------
 // choix ID pour l'affichage des infos des dispositifs
 // idx : idx de Domoticz    idm : idm de monitor (dans ce cas la table "dispositifs" 
@@ -49,14 +49,14 @@ define('CHOIXID','idm');// DZ:idm ou idx ; HA : idm uniquement
 define('NUMPLAN','2');//DZ uniquement: n° du plan regroupant tous les capteurs
 // parametres serveur DBMaria
 define('SERVEUR','localhost');
-define('MOTDEPASSE','Idem4546');
+define('MOTDEPASSE','xxxxxxx');
 define('UTILISATEUR','michel');
 define('DBASE','monitor');
 //------Page  Alarmee & Administration------------
 // page Alarme
 define('ON_ALARM',true);// affichage pour utilisation de l'alarme
 // mot passe alarme et administation , la page administration est ON
-define('PWDALARM','004546');//mot passe alarme
+define('PWDALARM','xxxxxxx');//mot passe alarme
 define('NOM_PASS_AL','pwdalarm');// nom du mot de passe dans la BD
 define('TIME_PASS_AL','3600');// temps de validité du mot de passe
 // ------------------------------------------------------------------------------------------
@@ -77,12 +77,12 @@ define('PILES', array( //id var domoticz, nom var domoticz, %1 (moyen), %2 (faib
 // Domoticz ou HA
 define('IPDOMOTIC', '192.168.1.76');//ip
 define('URLDOMOTIC', 'http://192.168.1.76:8086/');//url
-define('DOMDOMOTIC', 'https://domoticz.la-truffiere.ovh');//domaine
+define('DOMDOMOTIC', 'https://domoticz.xxxxxxxxxxxxxxxxxxxxxxxx.ovh');//domaine
 define('TOKENDOMOTIC1', '');//TOKEN ou BEARER
 define('IPDOMOTIC1', '');//ip 2emme serveur Domotique
 define('URLDOMOTIC1', 'http://192.168.1.5:8123/');//url ex:http://192.168.1.5:8123/
-define('DOMDOMOTIC1', 'https://ha.la-truffiere.ovh');//domaine
-define('TOKEN_DOM1',"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI3NzE0ZGY2ZDRlZmU0ZWQzYmI4MmI5YTRmN2NlM2UxYSIsImlhdCI6MTY3ODg4ODc1MSwiZXhwIjoxOTk0MjQ4NzUxfQ.UvvrBR60YRqHGqeYZV76nChlWoS1pW-evPJTP4Fcg2k");   
+define('DOMDOMOTIC1', 'https://ha.xxxxxxxxxxxxxxxxx.ovh');//domaine
+define('TOKEN_DOM1',"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");   
 
 //*************************Pour Domoticz
 define('VARTAB', URLDOMOTIC.'modules_lua/string_tableaux.lua');//
@@ -98,7 +98,7 @@ define('FICVARDZ','var_dz');//fichier json sauvegarde des variables
 // Page Météo  meteo concept
 define('ON_MET',true);// affichage page TOKEN PBLIGATOIRE
 // ---Token & code insee
-define('TOKEN_MC','2fce16877b45b86ba110ef2cdbf8d0e437563395f7a8ab2961919a7065ea2cd0');
+define('TOKEN_MC','xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
 define('INSEE','24454');
 // Alertes Pluie de  Météo France
 // Token
@@ -113,30 +113,30 @@ define('ON_GRAPH',true);// mise en service page graphique
 // Page MUR de Caméras-------------------------------------------
 // utilisation du mur :true sinon false , Nom du mur , nb caméras
 define('ON_MUR',true);// mise en service MUR
-define('NOMMUR','La Truffiere');// nom du mur
+define('NOMMUR','xxxxxxxxxxxxxxx');// nom du mur
 define('NBCAM','8');// nombre caméras
 // Zoneminder
 define('ZMURL','http://192.168.1.23/zm');//IP/zm
-define('ZMURLTLS','https:zoneminder.la-truffiere.ovh');// sous domaine
+define('ZMURLTLS','https:zoneminder.xxxxxxxxxxxxxxxxx.ovh');// sous domaine
 define('ZMUSER','michel');// pour mur_cameras.php
-define('ZMPASS','Idem4546');// pour mur_cameras.php
+define('ZMPASS','xxxxxxxxxxx');// pour mur_cameras.php
 define('TIMEAPI','3400');//suivant la valeur indiquée dans zoneminder
 //---------------------------------------------------------------------
 // Page zigbee2mqtt
 define('ON_ZIGBEE',true);// mise en service Zigbee
 define('IPZIGBEE', 'http://192.168.1.92:8084');//ip:port
-define('URLZIGBEE', 'https://zigbee.la-truffiere.ovh');//url
+define('URLZIGBEE', 'https://zigbee.la-truffxxxxxxxxxxxxx.ovh');//url
 //Page zwavejs2mqtt
 define('ON_ZWAVE',true);// mise en service Zwave
 define('IPZWAVE', 'http://192.168.1.76:8091');
-define('URLZWAVE', 'https://zwave.la-truffiere.ovh');//url');
+define('URLZWAVE', 'https://zwave.xxxxxxxxxxxxxxxx.ovh');//url');
 // Page Monitoring
 //Nagios
 define('ON_NAGIOS',true);// mise en service Monitoring
 define('IPNAGIOS', '192.168.1.8/nagios');//ip/dossier
-define('URLNAGIOS', 'https://monitoring.la-truffiere.ovh/nagios/');
+define('URLNAGIOS', 'https://monitoring.xxxxxxxxxxxxxx.ovh/nagios/');
 define('NAUSER', 'nagiosadmin');
-define('NAPASS', 'Idem4546');
+define('NAPASS', 'xxxxxxxxxxxxxxx');
 //Page Mur de Caméras avec Agent DVR
 //Agent DVR
 define('ON_DVR',false);// mise en service agent DVR
@@ -156,6 +156,7 @@ define('ECRANSPA', array(
 define('ON_RECETTES',true);
 //----------------------------------------------------------------
 //fichiers divers
+define('IPRPI', '192.168.1.8');//IP du Raspberry
 define('MSMTPRC_LOC_PATH', '/var/www/html/monitor/scripts/');//copie config serveur mail
 ?>
 
