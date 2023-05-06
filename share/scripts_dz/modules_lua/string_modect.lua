7f97a3ace25183e2d8cf2e73abfdee907f02f67a
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-05-06 18:35:36 +0200

    Update string_modect.lua

--- share/scripts_dz/modules_lua/string_modect.lua
+++ share/scripts_dz/modules_lua/string_modect.lua
@@ -1,1 +1,1 @@
-cam_modect = {[2]="http://michel:Idem4546@192.168.1.11/webcapture.jpg?command=snap&channel=1"}
+cam_modect = {[2]="http://michel:MOT_PASSE@192.168.1.11/webcapture.jpg?command=snap&channel=1"}
