564bbfd40927804ef7c4a71cc973f5b5cdbd5e14
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-05-06 18:38:34 +0200

    Update connect.py

--- share/scripts_dz/py/connect.py
+++ share/scripts_dz/py/connect.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3 -*- coding: utf-8 -*-
-login_free='MTI4MTI2MjA='
-pass_free='MkZRVE1NN3g0MmtzcHI='
+login_free='xxxxxxxxxxxx='
+pass_free='xxxxxxxxxxxxxx'
 ip_monitor='192.168.1.9'
 ip_mqtt='192.168.1.42'
 ip_tv='192.168.1.24'
