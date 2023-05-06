5da72de8987038b8acf9f78e53b812f075c5c6f1
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-05-06 18:46:08 +0200

    Update pushover.py

--- share/scripts_dz/py/pushover.py
+++ share/scripts_dz/py/pushover.py
@@ -2,8 +2,8 @@
 import requests,sys
 x= str(sys.argv[1])
 r = requests.post("https://api.pushover.net/1/messages.json", data = {
-"token": "asa28r7g15o8o28mgvufyc7ny4rxka",
-"user": "uoj2ks6quy86rpn51bmuv6ageau6ji",
+"token": "xxxxxxxxxxxxxxxxxxx",
+"user": "xxxxxxxxxxxxxxxxxxxxx",
 "message": x
 })
 print(r.text) 
