42fa7494374df5557f1620afa424b3aa57d91231
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-05-15 18:04:54 +0200

    Add files via upload

--- install/ct/monitor-install.sh
+++ install/ct/monitor-install.sh
@@ -120,6 +120,8 @@ msg_info "Installing Dependencies"
 $STD apt-get install -y curl
 $STD apt-get install -y sudo
 $STD apt-get install -y mc
+$STD apt-get install python3-pip
+$STD apt-get install git
 msg_ok "Installed Dependencies"
 
 
