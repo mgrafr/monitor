829c839c687844f09ee365e711e56b17db45c57b
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-05-24 22:41:29 +0200

    Update vto2000.lua

--- share/scripts_dz/lua/vto2000.lua
+++ share/scripts_dz/lua/vto2000.lua
@@ -1,14 +1,15 @@
 --vto2000 Dahua exploiter le changement de valeur d' une variable 
 -- pour signaler l' appui sur le portier video vto2000
 -- ATTENTION PORT=8086 , en cas de modification modifier le script pushover_img.sh
--- l'IP est reconnue automatique en cas de modification de serveur
+package.path = package.path..";www/modules_lua/?.lua"
+require 'connect'
 
 commandArray = {}
 --
 -- 
 if (uservariables['sonnette']=="1") then 
 --          --envoi image pushover ---------------
-            os.execute("/bin/bash userdata/scripts/bash/pushover_img.sh >> /home/michel/push.log 2>&1");
+            os.execute("/bin/bash userdata/scripts/bash/pushover_img.sh "..ip_domoticz..">> /home/michel/push.log 2>&1");
             commandArray['Variable:sonnette'] = '0'
 --        
 --    
