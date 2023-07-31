76373a2fd3127367c74f97f2f9649e9814c31c23
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-07-31 13:56:42 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -19,12 +19,16 @@ extensions = [
     'sphinx_rtd_theme',
 ]
 
-html_theme = "sphinx_rtd_theme"
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 
 html_theme = "sphinx_rtd_theme"
 html_theme_path = ["_themes", ]
+# The name for this set of Sphinx documents.  If None, it defaults to
+# "<project> v<release> documentation".
+html_title = "Monitor"
+
+# A shorter title for the navigation bar.  Default is the same as html_title.
+html_short_title = Monitor"
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = "images/logo.png"
