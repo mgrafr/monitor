763b5f3d663d37c79e20b1b9bb40b72030fc5e5e
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-07-31 14:26:00 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,7 @@ extensions = [
 
 html_theme = "sphinx_rtd_theme"
 html_theme_path = ["_themes", ]
+html_static_path = ["_static"]
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
 html_title = "Monitor"
