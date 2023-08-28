afae18176b6fbea0dc0249ecf96331c633df5027
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-08-28 09:19:32 +0200

    Add files via upload

--- include/thermometre_svg.php
+++ include/thermometre_svg.php
@@ -4,5 +4,9 @@
 	c-1.9,1.1-2.6,3.6-1.5,5.5s3.6,2.6,5.5,1.5c1.9-1.1,2.6-3.6,1.5-5.5c-0.4-0.6-0.9-1.1-1.5-1.5V4c0-1.1-0.9-2-2-2S8,2.9,8,4V10.5
 	L8,10.5z M6,9.5V4c0-2.2,1.8-4,4-4s4,1.8,4,4v5.5c2.5,2.2,2.7,6,0.5,8.5c-1.1,1.3-2.8,2-4.5,2c-3.3,0-6-2.7-6-6
 	C4,12.3,4.7,10.7,6,9.5z"/></a>
-<text id="temp_ext_cuisine" transform="matrix(0.6725 0 0 1 7.4663 15.254)" class="st33 st36b">tmp</text>
+<text id="temp_ext_cuisine" transform="matrix(0.6725 0 0 1 6 15)" class="st36b">tmp</text>
+<ellipse style="mix-blend-mode:darken;fill:#ff0000;fill-opacity:0;stroke:#cef6ff;stroke-width:1.00157;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:0"
+   class="cercle" id="cercle_23" cx="6" cy="6" rx="6" ry="6">
+   <title id="temperaure-exterieure">cercle temp ext cuisine</title>
+</ellipse>
 </svg>
