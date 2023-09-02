4bd29919f835e1a0b6aa0d7c8044455974399d00
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-09-02 11:02:09 +0200

    Add files via upload

--- share/scripts_PI8/envoi_sms.sh
+++ share/scripts_PI8/envoi_sms.sh
@@ -3,11 +3,11 @@
 ### Pour SMS Free
 CURL=/usr/bin/curl
 URL=https://smsapi.free-mobile.fr/sendmsg
-PASS=xxxxxxxxxxxxxxxxx
-USER=xxxxxxxxxxx
-# notification
+PASS=xxxxxxxxxx
+USER=wwwwwwwwww
+# notification exemple texte_notification num_tel1 num_tel2 num_tel3 ......
 not="$*"
 # envoi SMS Free
-$CURL -k -X POST "https://smsapi.free-mobile.fr/sendmsg?user=$USER&pass=$PASS&msg=$not"
+#$CURL -k -X POST "https://smsapi.free-mobile.fr/sendmsg?user=$USER&pass=$PASS&$
 ### envoi par modem GSM
 python3 /home/michel/envoi_sms.py $not >> /home/michel/erreur 2>&1
