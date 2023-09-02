7e1b9a208d4b871bb2a45eae2356383b003704cb
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-09-02 15:33:58 +0200

    Add files via upload

--- install/update.bash
+++ install/update.bash
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 cd /tmp
+mkdir monitor-update
 wget https://github.com/mgrafr/monitor/archive/refs/heads/update.zip
-unzip /tmp/monitor-update 
-# cp -r /tmp/monitor-update/* /www/monitor/
+unzip /tmp/update.zip 
+cp -ru /tmp/monitor-update/* /www/monitor/
 
 
