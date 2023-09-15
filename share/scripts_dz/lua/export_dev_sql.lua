13cae1d05e29b34ee733bac5ef1b608f521ddb63
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-09-15 09:08:31 +0200

    maj

--- share/scripts_dz/lua/export_dev_sql.lua
+++ share/scripts_dz/lua/export_dev_sql.lua
@@ -5,8 +5,7 @@ export_dev_sql]]
 package.path = package.path..";www/modules_lua/?.lua"
 require 'datas'
 require 'string_tableaux'
-data0=pression;data1=d_linky
---xxx="michel"; os.execute("python3 scripts/python/pushover.py "..xxx.." > /home/michel/fab.log 2>&1");
+data0=pression;
 year 	= tonumber(os.date("%Y"));
 month 	= os.date("%m");
 day 	= os.date("%d");
@@ -17,31 +16,23 @@ weekday = tonumber(os.date("%w"));
 time    = os.date("%X");
 datetime = year.."-"..month.."-"..day.." "..time;
 
-function write_datas(data0,data1)
+function write_datas(data0)
 f = io.open("www/modules_lua/datas.lua", "w")
-f:write('pression='..data0..';d_linky='..data1)
+f:write('pression='..data0)
 f:close()
 end
-function envoi_fab(libelle,valeur)
-    don=" "..libelle.."#"..valeur.."#"..datetime
-	print ("maj valeur:"..don);
-        local command = "/bin/bash userdata/scripts/bash/./fabric.sh"..don.." > /home/michel/fab.log 2>&1";
-        os.execute(command);
-        --txt="michel";os.execute("python3 scripts/python/pushover.py "..txt.." >> /home/michel/push.log 2>&1");
-end
+
 function round(num,numDecimal)
    local mult = 10^(numDecimal or 0)
   return math.floor(num * mult + 0.5) / mult
  end
---[[function Split(s, delimiter)
-    result = {};
-    for match in (s..delimiter):gmatch("(.-)"..delimiter) do
-        table.insert(result, match);
-    end
-    return result;
-end 
-]]
---
+
+function envoi_fab1(libelle,valeur)
+    don=" "..libelle.."#"..valeur.."#"..datetime
+	print("maj valeur:"..don);
+        command = "/bin/bash userdata/scripts/bash/./fabric.sh "..don.." > /home/michel/fab.log 2>&1";
+        os.execute(command);
+end
 return {
 	on = {
 		devices = {
@@ -63,49 +54,50 @@ return {
         if (item.name=='temp_cuisine_ete') then 
         -- choix nb decimales apres la virgule 
         -- local temp=round(deviceValue, 1)
-            local valeur=round(item.temperature, 0)
-            if (domoticz.variables('temp_cuis_ete').value ~= tostring(valeur)) then
-            domoticz.variables('temp_cuis_ete').set(tostring(valeur)) 
+           valeur=tostring(round(item.temperature, 0))
+            if (domoticz.variables('temp_cuis_ete').value ~= valeur) then
+            domoticz.variables('temp_cuis_ete').set(valeur) 
     	    libelle="temp_cuis_ete#valeur";
-    	    envoi_fab(libelle,valeur)    
+    	    envoi_fab1(libelle,valeur) 
             end       
         elseif (item.name=='temp_cave') then 
            --local valeur=round(item.temperature, 1)
-           local valeur=round(item.temperature, 0)
+          valeur=tostring(round(item.temperature, 0))
             if tostring(valeur)~=domoticz.variables('temp_cave').value then
             domoticz.variables('temp_cave').set(tostring(valeur))
     	    libelle="temp_cave#valeur";
-    	    envoi_fab(libelle,valeur) 
+    	   envoi_fab1(libelle,valeur) 
             end  
        elseif (item.name=='temp_cellier') then 
         -- local valeur=round(deviceValue, 1)
-        local valeur=round(item.temperature, 0)
+           valeur=tostring(round(item.temperature, 0))
             if tostring(valeur)~=domoticz.variables('temp_cellier').value then
             domoticz.variables('temp_cellier').set(tostring(valeur))    
     	    libelle="temp_cellier#valeur";
-            envoi_fab(libelle,valeur) 
+            envoi_fab1(libelle,valeur) 
             end
        elseif (item.name=='TempHumBaro') then 
-            local valeur=round(item.temperature, 0)
-            if tostring(valeur)~=domoticz.variables('temp_meteo').value then
-            domoticz.variables('temp_meteo').set(tostring(valeur))   
+            valeur=tostring(round(item.temperature, 0))
+            if valeur~=domoticz.variables('temp_meteo').value then
+            domoticz.variables('temp_meteo').set(valeur)   
     	    libelle="temp_meteo#valeur";
-            envoi_fab(libelle,valeur) 
+    	    envoi_fab1(libelle,valeur) 
             end
        elseif (item.name=='pir_salon_temp') then
-        local valeur=round(item.temperature, 0) 
+        valeur=tostring(round(item.temperature, 0))
         if tostring(valeur)~=domoticz.variables('temp_salon').value then
            domoticz.variables('temp_salon').set(tostring(valeur))    
             libelle="temp_salon#valeur";
-            envoi_fab(libelle,valeur) 
+           envoi_fab1(libelle,valeur)  
         end
        elseif (item.name=='pir ar cuisine_temp') then 
-         local valeur=round(item.temperature, 0)
+        valeur=tostring(round(item.temperature, 0))
         if tostring(valeur)~=domoticz.variables('temp_ar_cuisine').value then
             domoticz.variables('temp_ar_cuisine').set(tostring(valeur))    
 	        libelle="temp_cuisine#valeur";
-            envoi_fab(libelle,valeur) 
+            envoi_fab1(libelle,valeur) 
         end
+       --[[
        elseif (item.name=='PH_Spa') then
         local valeur=round(item.value, 1)
 	    libelle="ph_spa#valeur";envoi_fab(libelle,valeur) 
@@ -121,13 +113,14 @@ return {
         t[c]=i;c=c+1;
         end
 	    libelle="debit_spa#valeur";valeur=t[0]
-	    envoi_fab(libelle,valeur)  
+	    envoi_fab(libelle,valeur) 
+	    --]]
        elseif (item.name=='pression_chaudière') then 
         pressionch=tonumber(item.pressure);
         print ("pression_chaudiere:"..pressionch.."--"..pression);
         if (pression~=pressionch) then 
             libelle="pression_chaudiere#valeur";valeur=tostring(item.pressure)
-            envoi_fab(libelle,valeur) 
+           envoi_fab1(libelle,valeur) 
             --donnees['pression']=tonumber(deviceValue)
             write_datas(tonumber(item.pressure),data1)
             --pression_chaudiere: variable du fichier 'string_tableaux'
@@ -139,6 +132,5 @@ return {
             end
         end 
       end 
-       print("device changed:"..item.name) 
     end       
    }
