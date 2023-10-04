ac596695ffc09b5f0a518799a9352c3ffe1cdaeb
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-10-04 18:44:47 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,7 @@
 #
 import sys
 import os
-
+import sphinx_rtd_theme
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
