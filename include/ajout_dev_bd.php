b0bd9fd855ead307bbe4b90d8f9825f5c99ff92d
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-10-11 17:42:14 +0200

    maj, sup espace input

--- include/ajout_dev_bd.php
+++ include/ajout_dev_bd.php
@@ -3,7 +3,7 @@
 
 
 
-<div id="adb" style="line-height: normal;font-size: 14px;"><p><span style="margin-left: (10)px;"><strong>Nom de l' Appareil :</strong> </span><input type="text" style="width:200px;margin-left:10px;" id="nom" value=" "  ><br><span style="color:red">*</span>champ requis&nbsp;&nbsp;<span style="color:green">*</span>choisir 1 seul des 2 champs</p>	
+<div id="adb" style="line-height: normal;font-size: 14px;"><p><span style="margin-left: (10)px;"><strong>Nom de l' Appareil :</strong> </span><input type="text" style="width:200px;margin-left:10px;" id="nom" value=""  ><br><span style="color:red">*</span>champ requis&nbsp;&nbsp;<span style="color:green">*</span>choisir 1 seul des 2 champs</p>	
 <p style="font-size:14px"> <em>type de mise à jour JS</em><span style="color:red">&nbsp;&nbsp;* </span><br>
 <input type="hidden"id="app" value="dev_bd">
 <input type="radio" name="type" value="control"> control
@@ -17,7 +17,7 @@
  <p style="font-size:14px"> <strong>Home Assistant</strong><br>
 <span style="margin-left: 10px;">device.ID : <input type="text" style="width:200px;margin-left:10px;" id="ha_id" value=""  ><span style="color:green">&nbsp;&nbsp;* </span>	 
 	 <p style="font-size:14px"> <strong>Monitor</strong><br>
-  <span style="margin-left: 10px;">Idm  : <input type="text" style="width:50px;" id="idm" value=" " ><span style="color:red">&nbsp;&nbsp;* </span></span><br>
+  <span style="margin-left: 10px;">Idm  : <input type="text" style="width:50px;" id="idm" value="" ><span style="color:red">&nbsp;&nbsp;* </span></span><br>
   <span style="margin-left: 10px;">Id1 html : <input type="text" style="width:120px;" id="var1" value="#"><span style="color:red">&nbsp;&nbsp;* </span></span><br>
    <span style="margin-left: 10px;">Id2 html :<input type="text" style="width:120px;margin-left: 10px;" id="var2" value="" ><br>
     <span style="margin-left: 10px;">Couleur Id1-Id2 : ON = <input type="text" style="width:200px;margin-left: 10px;" id="coula" value="" ><br>
--- include/ajout_var_bd.php
+++ include/ajout_var_bd.php
@@ -3,18 +3,18 @@
 echo '<div id="avb" style="line-height: normal;"><span style="color:red">*</span>champ requis
 	<p style="font-size:14px"> <strong>Domoticz</strong><p>
 <input type="hidden" id="app"  value="var_bd"><input type="hidden" id="command"  value="1">
- <span style="margin-left: 10px;">Idx : <input type="text" style="width:50px;margin-left:10px;" value=" " id="idx" ></span><span style="color:red">&nbsp;&nbsp;* </span><br>
- <span style="margin-left: 10px;">Nom  :<input type="text" style="width:200px;margin-left: 3px;" id="name" value=" " ></span><br>
+ <span style="margin-left: 10px;">Idx : <input type="text" style="width:50px;margin-left:10px;" value="" id="idx" ></span><span style="color:red">&nbsp;&nbsp;* </span><br>
+ <span style="margin-left: 10px;">Nom  :<input type="text" style="width:200px;margin-left: 3px;" id="name" value="" ></span><br>
  <p style="font-size:14px"> <strong>Home Assistant</strong><p>
  <input type="hidden" id="app"  value="var_bd"><input type="hidden" id="command"  value="1">
- <span style="margin-left: 10px;">device ID : <input type="text" style="width:200px;margin-left:10px;" value=" " id="ha_id" ></span><span style="color:red">&nbsp;&nbsp;* </span><br>
- <span style="margin-left: 10px;">Nom usuel :<input type="text" style="width:200px;margin-left: 3px;" id="nom" value=" " ></span><br>
+ <span style="margin-left: 10px;">device ID : <input type="text" style="width:200px;margin-left:10px;" value="" id="ha_id" ></span><span style="color:red">&nbsp;&nbsp;* </span><br>
+ <span style="margin-left: 10px;">Nom usuel :<input type="text" style="width:200px;margin-left: 3px;" id="nom" value="" ></span><br>
   <p style="font-size:14px"> <strong>Base de données SQL</strong><p>
-  <span style="margin-left: 10px;">Id Image : <input type="text" style="width:120px;"  id="id_img" value=" " ></span><br>
-   <span style="margin-left: 10px;">Id Texte :<input type="text" style="width:120px;margin-left: 10px;" id="id_txt" value=" " ></span><br>
+  <span style="margin-left: 10px;">Id Image : <input type="text" style="width:120px;"  id="id_img" value="" ></span><br>
+   <span style="margin-left: 10px;">Id Texte :<input type="text" style="width:120px;margin-left: 10px;" id="id_txt" value="" ></span><br>
   <p style="font-size:14px"> <em>texte -> image</em><p><br>
-  <span style="margin-left: 10px;">Texte : <input type="text" style="width:200px;margin-left: 10px;" id="texte_bd" value=" " ></span><br>
-   <span style="margin-left: 10px;">Image :<input type="text" style="width:200px;margin-left:9px;" id="image_bd" value=" " ></span><br>
+  <span style="margin-left: 10px;">Texte : <input type="text" style="width:200px;margin-left: 10px;" id="texte_bd" value="" ></span><br>
+   <span style="margin-left: 10px;">Image :<input type="text" style="width:200px;margin-left:9px;" id="image_bd" value="" ></span><br>
  <button type="button" onclick="adby(1)" id="bouton_avb" style="width:100px;height:30px">Envoi</button>  
 </div>';
 
