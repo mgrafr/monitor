0d27b215d90ff94c0f463dfed20b7c8a70af0f94
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-10-28 19:19:41 +0200

    Add files via upload

---
+++ share/scripts_dz/modules_lua/table_zb_zw.lua
@@ -0,0 +1,57 @@
+liste_ls={}
+
+liste_ls[1]={["idx"]=
+"109",["name"]="prise sejour",["idm"]="2",["lastseen"]="non"}
+
+liste_ls[2]={["idx"]=
+"134",["name"]="pir ar cuisine_temp",["idm"]="21",["lastseen"]="non"}
+
+liste_ls[3]={["idx"]=
+"146",["name"]="temp_cave",["idm"]="5",["lastseen"]="oui"}
+
+liste_ls[4]={["idx"]=
+"148",["name"]="temp_cellier",["idm"]="6",["lastseen"]="oui"}
+
+liste_ls[5]={["idx"]=
+"156",["name"]="temp_cuisine_ete",["idm"]="4",["lastseen"]="oui"}
+
+liste_ls[6]={["idx"]=
+"177",["name"]="volet_bureau_m",["idm"]="31",["lastseen"]="non"}
+
+liste_ls[7]={["idx"]=
+"129",["name"]="porte entree",["idm"]="38",["lastseen"]="non"}
+
+liste_ls[8]={["idx"]=
+"154",["name"]="porte ar cuisine",["idm"]="35",["lastseen"]="non"}
+
+liste_ls[9]={["idx"]=
+"150",["name"]="temp_ext",["idm"]="23",["lastseen"]="oui"}
+
+liste_ls[10]={["idx"]=
+"113",["name"]="pir ar cuisine_motion",["idm"]="20",["lastseen"]="non"}
+
+liste_ls[11]={["idx"]=
+"265",["name"]="porte_fenetre",["idm"]="7",["lastseen"]="non"}
+
+liste_ls[12]={["idx"]=
+"317",["name"]="pir_entree_motion",["idm"]="25",["lastseen"]="non"}
+
+liste_ls[13]={["idx"]=
+"320",["name"]="pir_entree_temperature",["idm"]="26",["lastseen"]="non"}
+
+liste_ls[14]={["idx"]=
+"233",["name"]="Sirene_battery_islow",["idm"]="72",["lastseen"]="non"}
+
+liste_ls[15]={["idx"]=
+"329",["name"]="detecteur fumee_smoke",["idm"]="33",["lastseen"]="non"}
+
+liste_ls[16]={["idx"]=
+"181",["name"]="volet_bureau_moving",["idm"]="90",["lastseen"]="oui"}
+
+liste_ls[17]={["idx"]=
+"307",["name"]="lampe_jardin",["idm"]="73",["lastseen"]="oui"}
+
+liste_ls[18]={["idx"]=
+"164",["name"]="prise_cuis_ete",["idm"]="74",["lastseen"]="oui"}
+
+nombre_enr=18
