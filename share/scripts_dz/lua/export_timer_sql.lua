2ff19fddf8bfff9f2337e32183509f100891add4
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-12-09 10:40:08 +0100

    Add files via upload

--- share/scripts_dz/lua/export_timer_sql.lua
+++ share/scripts_dz/lua/export_timer_sql.lua
@@ -5,8 +5,19 @@ month 	= os.date("%m");
 day 	= os.date("%d");
 day 	= os.date("%d");
 time    = os.date("%X");
+
 datetime = year.."-"..month.."-"..day.." "..time;
---
+local function split(s, delimiter)
+	local result = {}
+	for match in (s..delimiter):gmatch('(.-)'..delimiter) do
+		table.insert(result, match)
+	end
+	return result
+end
+--function tointeger( x )
+--    num = tonumber( x )
+--    return num < 0 and math.ceil( num ) or math.floor( num )
+--end
 function envoi_fab(don)
         local command = "/bin/bash userdata/scripts/bash/./fabric.sh"..don.." > /home/michel/fab.log 2>&1";
         os.execute(command);
@@ -16,18 +27,34 @@ function round(num,numDecimal)
   return math.floor(num * mult + 0.5) / mult
  end
 --
+local scriptVar = 'linky_sql'
 return {
-	on = {
-        timer = {
-             'at 09:00'
-        }
-    },
- execute = function(domoticz, item)
-    domoticz.log('export sql: ' .. item.trigger)
-       local Counter = domoticz.devices('linky_bis').counter; print("compteur_kwh:"..Counter)
-       local Usage = domoticz.devices('linky_bis').usage;    print("compteur_kw:"..Usage) 
-       libelle="energie#conso"
-       don=" "..libelle.."#"..tostring(round(Counter,1)).."#"..datetime.."#pmax#"..tostring(round(Usage/1000,1)); print("energie"..don);
-       envoi_fab(don)--
+    on = { 
+        timer =  {'at 10:30'}, 
+        httpResponses = { scriptVar }},
+        logging = { level = domoticz.LOG_ERROR, marker  = scriptVar },
+    
+    execute = function(dz, item) 
+        if (item.isTimer) then
+            local url = 'http://127.0.0.1:8080/json.htm?type=command&param=getdevices&rid=427';
+            print(url);
+            dz.openURL({ 
+                url = url, 
+                method = 'GET',
+                callback = scriptVar, })
+        end
+        if (item.isHTTPResponse ) then
+            local results = item.json.result
+            -- loop through the nodes and print some info
+            for i, node in pairs(results) do
+            print('Data'.. node.Data);m=split(node.Data, ';')
+            local mCounter = m[1] ; print("compteur_kwh:"..mCounter);--mCounter=tointeger(mCounter)/1000;
+            local mUsage = m[5] ;    print("compteur_kw:"..mUsage) ;
+            
+            libelle="energie#conso"
+            don=" "..libelle.."#"..tostring(round(tonumber(mCounter)/1000,1)) .."#"..datetime.."#pmax#"..tostring(round(tonumber(mUsage)/1000,1)); print("energie"..don);
+            envoi_fab(don)
+            end
+        end
 end
 }
