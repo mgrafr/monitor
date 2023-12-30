2a28978750e3655992d1d556c0e713df0ff2fa7a
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-12-30 15:27:47 +0100

    Add files via upload

--- bd_sql/messages.sql
+++ bd_sql/messages.sql
@@ -1,23 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 5.1.4deb1~bpo11+1
--- https://www.phpmyadmin.net/
---
--- Hôte : localhost:3306
--- Généré le : sam. 11 nov. 2023 à 17:33
--- Version du serveur : 10.5.21-MariaDB-0+deb11u1
--- Version de PHP : 8.2.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
 -- Base de données : `monitor`
 --
 
@@ -40,19 +25,13 @@ CREATE TABLE `messages` (
 -- Déchargement des données de la table `messages`
 --
 
---
--- Index pour les tables déchargées
---
+INSERT INTO `messages` (`num`, `nom`, `id1_html`, `contenu`, `maj`, `last_update`) VALUES
+(1, 'message1', 'lastseen1', 'essai', 0, '2023-11-10 17:25:10');
 
---
 -- Index pour la table `messages`
 --
 ALTER TABLE `messages`
   ADD PRIMARY KEY (`num`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
 
 --
 -- AUTO_INCREMENT pour la table `messages`
@@ -61,6 +40,3 @@ ALTER TABLE `messages`
   MODIFY `num` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
