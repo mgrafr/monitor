e3b41f980e0b2eccae65ab34a95fa867a2aa12a1
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-12-31 18:13:15 +0100

    Update maj_2.2.7.sh

--- install/maj_2.2.7.sh
+++ install/maj_2.2.7.sh
@@ -3,6 +3,8 @@ wget https://raw.githubusercontent.com/mgrafr/monitor/main/css/mes_css.css
 mv mes_css.css css/mes_css.css
 wget https://raw.githubusercontent.com/mgrafr/monitor/main/install/update.bash
 mv update.bash install/update.bash
+rm .version
+wget https://raw.githubusercontent.com/mgrafr/monitor/main/.version
 chown www-data:www-data /var/www/html/monitor/admin/config.php
 echo "-----------------------------------------------------------------"
 echo "-----------Mise à jour vers la version 2.2.7.1-------------------"
