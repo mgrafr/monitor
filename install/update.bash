27eca194e712ad7148472550660d4bd4ff423ea8
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-12-31 14:47:57 +0100

    Update update.bash

--- install/update.bash
+++ install/update.bash
@@ -1,9 +1,14 @@
 #!/usr/bin/bash
 
-cd /tmp
-mkdir monitor-update
-wget https://github.com/mgrafr/monitor/archive/refs/heads/update.zip
-unzip /tmp/update.zip 
-cp -ru /tmp/monitor-update/* /www/monitor/
+cd /www/monitor
+version=$(head -n 1 /var/www/html/monitor/.version)
+echo $version
+if [[ $version=2.2.7 ]] 
+then
+wget https://raw.githubusercontent.com/mgrafr/monitor/main/install/maj_2.2.7.sh
+chmod +x maj_2.2.7.sh
+./maj_2.2.7.sh
+else 
+echo " *************pas de maj**********************"
 
 
