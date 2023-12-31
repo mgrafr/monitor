dc1a5ec56e04dd4b732d93dd2704c79769d1fea0
domo-site <93765654+mgrafr@users.noreply.github.com>
2023-12-31 18:47:42 +0100

    Update update.bash

--- install/update.bash
+++ install/update.bash
@@ -3,13 +3,13 @@
 cd /www/monitor
 version=$(head -n 1 /var/www/html/monitor/.version)
 echo $version
-if [[ $version=2.2.7 ]] 
+if [[ $version == 2.2.7 ]] 
 then
 wget https://raw.githubusercontent.com/mgrafr/monitor/main/install/maj_2.2.7.sh
 chmod +x maj_2.2.7.sh
 ./maj_2.2.7.sh
 rm  maj_2.2.7.sh
-elif [[ $version=2.2.7.1 ]] 
+elif [[ $version == 2.2.7.1 ]] 
 then
 wget https://raw.githubusercontent.com/mgrafr/monitor/main/install/maj_2.2.7.1.sh
 chmod +x maj_2.2.7.1.sh
