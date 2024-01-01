3afcee80351e950bb330f776baa6f5c0484ac895
domo-site <93765654+mgrafr@users.noreply.github.com>
2024-01-01 11:11:14 +0100

    Update maj_2.2.7.1.sh

--- install/maj_2.2.7.1.sh
+++ install/maj_2.2.7.1.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/bash
+wget https://raw.githubusercontent.com/mgrafr/monitor/main/api/f_pour_api.php
+mv f_pour_api.php api/f_pour_api.php
+wget https://raw.githubusercontent.com/mgrafr/monitor/main/api/json.php
+mv json.php api/json.php
+rm .version
+rm install/maj_2.2.7.1.sh
+wget https://raw.githubusercontent.com/mgrafr/monitor/main/.version
+echo "-----------------------------------------------------------------"
+echo "--------Mise à jour vers la version 2.2.7.1 terminée-------------"
+echo "-----------------------------------------------------------------"
 
-echo "-----------Pas de mises à jour pour la version 2.2.7.1-------------------"
