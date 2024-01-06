1afa11a25aa6c2ae6a81d70b5bd10d3812c0fdd8
domo-site <93765654+mgrafr@users.noreply.github.com>
2024-01-06 16:20:11 +0100

    Add files via upload

--- install/maj_2.2.7.1.sh
+++ install/maj_2.2.7.1.sh
@@ -19,8 +19,6 @@ wget https://raw.githubusercontent.com/mgrafr/monitor/main/include/footer.php
 #
 rm .version
 wget https://raw.githubusercontent.com/mgrafr/monitor/main/.version
-wget https://raw.githubusercontent.com/mgrafr/monitor/main/install/update.bash
-mv update.bash install/update.bash
 echo "-----------------------------------------------------------------------"
 echo "-----------Mises à jour vers la version 2.2.7.3 terminées--------------"
 echo "-----------------------------------------------------------------------"
--- install/maj_2.2.7.3.sh
+++ install/maj_2.2.7.3.sh
@@ -8,8 +8,6 @@ wget https://raw.githubusercontent.com/mgrafr/monitor/main/include/footer.php
 #
 rm .version
 wget https://raw.githubusercontent.com/mgrafr/monitor/main/.version
-wget https://raw.githubusercontent.com/mgrafr/monitor/main/install/update.bash
-mv update.bash install/update.bash
 echo "-----------------------------------------------------------------------"
 echo "-----------Mises à jour vers la version 2.2.7.4 terminées--------------"
 echo "-----------------------------------------------------------------------"
