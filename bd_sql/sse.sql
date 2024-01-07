54302b295aeb1790b7814852384e53ed4cf76159
domo-site <93765654+mgrafr@users.noreply.github.com>
2024-01-07 14:13:51 +0100

    Add files via upload

--- bd_sql/sse.sql
+++ bd_sql/sse.sql
@@ -1,27 +1,14 @@
--- phpMyAdmin SQL Dump
--- version 5.1.4deb1~bpo11+1
--- https://www.phpmyadmin.net/
---
--- Hôte : localhost:3306
--- Généré le : mer. 27 déc. 2023 à 14:43
--- Version du serveur : 10.5.21-MariaDB-0+deb11u1
--- Version de PHP : 8.2.13
+-
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de données : `monitor`
 --
-
--- --------------------------------------------------------
 
 --
 -- Structure de la table `sse`
@@ -30,17 +17,15 @@ SET time_zone = "+00:00";
 CREATE TABLE `sse` (
   `num` int(1) NOT NULL,
   `id` varchar(20) NOT NULL,
-  `state` varchar(5) NOT NULL
+  `state` varchar(5) NOT NULL,
+  `date` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `sse`
 --
 
-INSERT INTO `sse` (`num`, `id`, `state`) VALUES
-(0, '0', '');
+INSERT INTO `sse` (`num`, `id`, `state`, `date`) VALUES
+(0, '', '', '09:42:44');
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
