b9655c8b65f389bfef44ab201a7ea6dbf7cd22f2
domo-site <93765654+mgrafr@users.noreply.github.com>
2024-01-30 17:54:27 +0100

    Add files via upload

--- include/fonctions_1.php
+++ include/fonctions_1.php
@@ -1,36 +1,41 @@
 <?php
-function sql_plan($t){// SERVEUR SQL connexion
+
+function sql_plan($t1,$s=""){// SERVEUR SQL connexion
 $conn = new mysqli(SERVEUR,UTILISATEUR,MOTDEPASSE,DBASE);
- if (($t!='0')  && (strlen($t) < 5)) {
-	$sql="SELECT * FROM `".DISPOSITIFS."` WHERE idx = '$t' AND maj_js <> 'variable';";
+ if ($t1=='3')  {
+	$sql="SELECT * FROM ".DISPOSITIFS." WHERE nom_objet = '".$s."' AND maj_js <> 'variable';";
 	$result = $conn->query($sql);
-	$row = $result->fetch_assoc();return $row;}
-else if ($t!='0'  && strlen($t) > 4) {
-	$sql="SELECT * FROM `".DISPOSITIFS."` WHERE ID = '$t' AND maj_js <> 'variable';";
+	$row = $result->fetch_assoc();
+	 return $row;}
+else if ($t1=='2') {
+	$sql="SELECT * FROM `".DISPOSITIFS."` WHERE ID = '$s' AND maj_js <> 'variable';";
+		$result = $conn->query($sql);//if ($result === FALSE) {echo "pas id";return "";}
+		$row = $result->fetch_assoc();return $row;
+      }
+else if ($t1=='1')  {
+	$sql="SELECT * FROM `".DISPOSITIFS."` WHERE idx = '$s' AND maj_js <> 'variable';";
 		$result = $conn->query($sql);//if ($result === FALSE) {echo "pas id";return "";}
 		$row = $result->fetch_assoc();
 	return $row;}
-else if ($t=='0') {//$commande="On";
-				   
-				   
+else if ($t1=='0') {//$commande="On";
 if (IPDOMOTIC1 != ""){
-	$sql="SELECT * FROM dispositifs WHERE (`maj_js` LIKE '%onoff%' AND `ID` <> '');";
+	$sql="SELECT * FROM dispositifs WHERE (`maj_js` LIKE '%on%' AND `maj_js` <> 'control' AND `ID` <> '');";
 	$result = $conn->query($sql);
 	while($row = $result->fetch_array(MYSQLI_ASSOC)){sql_1($row,'turnonoff','ha');				  
 	}				  
 					 }
 if (IPDOMOTIC != ""){
-	$sql="SELECT * FROM dispositifs WHERE (`maj_js` LIKE '%onoff%' AND `idx` <> '');";
+	$sql="SELECT * FROM dispositifs WHERE (`maj_js` LIKE '%on%' AND `maj_js` <> 'control' AND `idx` <> '');";
 	$result = $conn->query($sql);//echo "/*";
 	while($row = $result->fetch_array(MYSQLI_ASSOC)){sql_1($row,'switchOnOff_setpoint','dz');
 		}
 	}
-	
 return;}
 else echo "pas d'id_dz";
 }
 function sql_1($row,$f,$ser_dom){
 $commande="On";
+if ($row['maj_js']=="on"){$commande="group on";}	
 if($ser_dom=="dz")$ser_dom=$row['idx'];
 if($ser_dom=="ha")$ser_dom=$row['ID'];	
 if($row['id1_html']!='' && $row['id1_html']!='#' ){$s='$("#'.$row["id1_html"];
