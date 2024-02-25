83e04d59b076bd482d42313bee236a7e74d32c2d
domo-site <93765654+mgrafr@users.noreply.github.com>
2024-02-25 14:03:43 +0100

    Add files via upload

--- include/alarmes.php
+++ include/alarmes.php
@@ -1,4 +1,23 @@
+<div class="modal" role="dialog" id="pwdalarm">
 
+
+		    <form class="form_al"><span class="close_clavier">x</span>
+        <input type="password" style="max-width: 140px;" id="password" /></br>
+        <input type="button" value="1" id="1" class="pinButton calc"/>
+        <input type="button" value="2" id="2" class="pinButton calc"/>
+        <input type="button" value="3" id="3" class="pinButton calc"/><br>
+        <input type="button" value="4" id="4" class="pinButton calc"/>
+        <input type="button" value="5" id="5" class="pinButton calc"/>
+        <input type="button" value="6" id="6" class="pinButton calc"/><br>
+        <input type="button" value="7" id="7" class="pinButton calc"/>
+        <input type="button" value="8" id="8" class="pinButton calc"/>
+        <input type="button" value="9" id="9" class="pinButton calc"/><br>
+        <input type="button" value="raz" id="clear" class="pinButton clear"/>
+        <input type="button" value="0" id="0 " class="pinButton calc"/>
+        <input type="button" value="envoi" id="enter" class="pinButton enter"/>
+      </form>
+
+</div>
 
 <!-- section Alarmes start -->
 <!-- ================ -->
