f85b791757a9c541ac0736637f993132777d5104
domo-site <93765654+mgrafr@users.noreply.github.com>
2024-02-26 14:07:27 +0100

    Add files via upload

---
+++ admin/connect/connect.lua
@@ -0,0 +1,13 @@
+
+login_free='53909849'
+pass_free='RClC4neFfomxqD'
+mail_gmail='gravier.michel@gmail.com'
+ip_monitor='192.168.1.9'
+ip_mqtt='192.168.1.42'
+ip_tv='192.168.1.24'
+ip_dz_localhost='127.0.0.1:8085'
+ip_domoticz='192.168.1.167:8085'
+ip_zoneminder='192.168.1.23'
+ip_sse='192.168.1.118'
+port_sse='3000'
+tel={'0670065886'}
