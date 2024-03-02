1e6ee4676a2ca40c227cc0d9614626d3198da2cf
domo-site <93765654+mgrafr@users.noreply.github.com>
2024-03-02 16:01:34 +0100

    Add files via upload

--- install/maj_3.0.2.sh
+++ install/maj_3.0.2.sh
@@ -8,13 +8,14 @@ read
 mkdir tmp
 wget -O tmp/maj.zip https://github.com/mgrafr/monitor/archive/refs/heads/miseajour.zip
 unzip tmp/maj.zip
-cp -RTu --update tmp/include include
-cp -RTu --update tmp/css  css
-cp -u --update tmp/fonctions.php  fonctions.php
+cp -RTu --update monitor-miseajour/include include
+cp -RTu --update monitor-miseajour/css  css
+cp -u --update monitor-miseajour/fonctions.php  fonctions.php
 mysql --user="root" --database="monitor"  -e "DROP TABLE IF EXISTS dispositifs;"
 mysql -root monitor < bd_sql/dispositifs.sql
 #
 rm -R tmp
+rm -R monitor-miseajour
 rm .version
 cp tmp/.version .version
 echo "-----------------------------------------------------------------------"
