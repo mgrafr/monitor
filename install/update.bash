6e739dfbcd6f76f15cf9e62494efb35bf88520cc
domo-site <93765654+mgrafr@users.noreply.github.com>
2024-03-02 16:47:05 +0100

    Add files via upload

--- install/update.bash
+++ install/update.bash
@@ -3,7 +3,11 @@
 cd /www/monitor
 version=$(head -n 1 /var/www/html/monitor/.version)
 echo $version
+apt update
+apt install dos2unix
+apt install unzip
 wget https://raw.githubusercontent.com/mgrafr/monitor/main/install/maj_$version.sh
+dos2unix maj_$version.sh
 chmod +x maj_$version.sh
 ./maj_$version.sh
 rm  maj_$version.sh
