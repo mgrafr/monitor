528ff0f6a59e3599d06c07dfc1cd5cbbb048effe
domo-site <93765654+mgrafr@users.noreply.github.com>
2024-03-02 15:42:32 +0100

    Add files via upload

--- install/maj_3.0.2.sh
+++ install/maj_3.0.2.sh
@@ -7,7 +7,7 @@ echo "appuyer sur une touche pour continuer"
 read
 mkdir tmp
 wget - O tmp/maj.zip https://github.com/mgrafr/monitor/archive/refs/heads/miseajour.zip
-unzip tmp/miseajour.zip
+unzip tmp/maj.zip
 cp -RTu --update tmp/include include
 cp -RTu --update tmp/css  css
 cp -u --update tmp/fonctions.php  fonctions.php
