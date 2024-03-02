778ee3f6250a93acd2e47c55de6de5d4195866a7
domo-site <93765654+mgrafr@users.noreply.github.com>
2024-03-02 15:24:58 +0100

    Add files via upload

--- install/maj_3.0.2.sh
+++ install/maj_3.0.2.sh
@@ -1,6 +1,22 @@
 !/usr/bin/bash
 
-echo "-----------------------version 3.0.2--------------------------------"
-echo "--------------------                         --------------------------"
-echo "-----------           PAS de mises à jour                --------------"
+echo "-----------------------------------------------------------------"
+echo "-----------Mise à jour vers la version 3.0.3  -------------------"
+echo "-----------------------------------------------------------------"
+echo "appuyer sur une touche pour continuer"
+read
+mkdir tmp
+wget - O tmp/maj.zip https://github.com/mgrafr/monitor/archive/refs/heads/miseajour.zip
+unzip tmp/maj.zip
+cp -RTu --update tmp/include include
+cp -RTu --update tmp/css  css
+cp -u --update tmp/fonctions.php  fonctions.php
+mysql --user="root" --database="monitor"  -e "DROP TABLE IF EXISTS dispositifs;"
+mysql -root monitor < bd_sql/dispositifs.sql
+#
+rm -R tmp
+rm .version
+cp tmp/.version .version
+echo "-----------------------------------------------------------------------"
+echo "-----------Mises à jour vers la version 3.0.3   terminées--------------"
 echo "-----------------------------------------------------------------------"
