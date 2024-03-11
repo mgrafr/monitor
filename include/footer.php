24c416b143346445339fbc57bb57dd5b47db05e3
domo-site <93765654+mgrafr@users.noreply.github.com>
2024-03-11 19:05:26 +0100

    Add files via upload

--- include/footer.php
+++ include/footer.php
@@ -431,7 +431,7 @@ function appel_admin(choix_admin,fenetre){
 	  data: "app=admin&variable="+choix_admin+"&command="+fenetre,
       success: function(data) {$(fenetre).empty();
 		document.getElementById(fenetre).innerHTML = data;document.getElementById(fenetre).style.display = "block";
-		if (data[3]=='Entrer votre mot de passe' || data[4]=='Entrer votre mot de passe' ){document.getElementById("d_btn_a").style.display = "block";
+		if (data=='Entrer votre mot de passe'){document.getElementById("d_btn_a").style.display = "block";
 		document.getElementById("d_btn_al").style.display = "block";}
 							},
 	  error: function() { 
