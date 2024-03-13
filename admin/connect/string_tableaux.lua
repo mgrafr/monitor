ceed31c279a278975ff6dd765837e6cd6f650b3b
domo-site <93765654+mgrafr@users.noreply.github.com>
2024-03-13 19:28:59 +0100

    Add files via upload

---
+++ admin/connect/string_tableaux.lua
@@ -0,0 +1,20 @@
+-- liste de variables
+-- string
+--jour_poubelle_grise="Wednesday"
+--jour_poubelle_jaune="Sunday"
+--semaine_poub_jaune= 0  -- 0 pour pair 1 pour impair
+-- exclusion poubelles j=jaune, g=grise
+--e_poubelles = {["31-12"]="j",["09-05"]="g",["19-05"]="j",["14-08"]="g"};
+-- ajout poubelles j=jaune, g=grise
+--a_poubelles = {["02-01"]="j",["21-05"]="j"};
+--
+--jour_fosse="26"
+--pression_chaudiere=0.8
+--alerte device off line ou mort
+--max_lastseen=24 
+--max_lastupdate=48 
+--max_bat=80 
+-- anniversaires
+--anniversaires = {["27-08"]="Damien",["18-05"]="Yoann"};
+
+
