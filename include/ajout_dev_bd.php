8ebb8b889b8dbe2e80327a213a9d32689c9230d9
domo-site <93765654+mgrafr@users.noreply.github.com>
2024-06-12 15:55:25 +0200

    Add files via upload

--- include/ajout_dev_bd.php
+++ include/ajout_dev_bd.php
@@ -16,11 +16,11 @@
 <input type="hidden" id="command"  value="2">
 <span style="margin-left: 10px;">Idx : <input type="text" style="width:50px;margin-left:10px;" id="idx" value=""  ><span style="color:green">&nbsp;&nbsp;* </span></p>
  <p style="margin-left: 10px;font-size:14px"> <strong>Home Assistant</strong><br>
-<span style="margin-left: 10px;">entity_id : <input type="text" style="width:200px;margin-left:10px;" id="ha_id" value=""  ><span style="color:green">&nbsp;&nbsp;* </span></p>	<p style="margin-left: 10px;font-size:14px"> <strong>Domoticz &Home Assistant </strong>(nom ou object_id)<br> <span style="margin-left: 10px;">Nom_objet  :<input type="text" style="width:200px;margin-left: 3px;" id="nom_objet" value=""></p>
+<span style="margin-left: 10px;">entity_id : <input type="text" style="width:200px;margin-left:10px;" id="ha_id" value=""  ><span style="color:green">&nbsp;&nbsp;* </span></p>	<p style="margin-left: 10px;font-size:14px"> <strong>Domoticz , HA & io.Broker </strong>(nom ou object_id)<br> <span style="margin-left: 10px;">Nom_objet  :<input type="text" style="width:200px;margin-left: 3px;" id="nom_objet" value=""></p>
 	<p style="margin-left: 10px;font-size:14px"> <strong>Monitor</strong><br>
   <span style="margin-left: 10px;">Idm  : <input type="text" style="width:50px;" id="idm" value="" ><span style="color:red">&nbsp;&nbsp;* </span><em style="font-size:13px">(1à999 Groupe:G1.. , Scene:S1..)</em></span><br>
   <input type="radio" name="actif" value="1" checked><span style="color:red">Actif </span>:&nbsp;&nbsp;<input type="radio" name="actif"  value="0"><span style="color:black">Inactif</span> : 
-   <input type="radio" name="actif" value="2" ><span style="color:blue">Dz </span>:&nbsp;&nbsp;<input type="radio" name="actif"  value="3"><span style="color:green">Ha</span><br>
+   <input type="radio" name="actif" value="2" ><span style="color:blue">Dz </span>&nbsp;&nbsp;<input type="radio" name="actif"  value="3"><span style="color:green">Ha</span>&nbsp;&nbsp;<input type="radio" name="actif"  value="4"><span style="color:purple">Iob</span><br>
   <span style="margin-left: 10px;">Id1 html : <input type="text" style="width:120px;" id="id1_html" value="#"><span style="color:red">&nbsp;&nbsp;* </span></span><br>
    <span style="margin-left: 10px;">Id2 html :<input type="text" style="width:120px;margin-left: 10px;" id="id2_html" value="" ><br>
     <span style="margin-left: 10px;">Couleur Id1-Id2 : ON = <input type="text" style="width:200px;height:20px;margin-left: 10px;" id="coula" value="" ><br>
