cf41db5897ea849f393cce00869355a0a4276803
domo-site <93765654+mgrafr@users.noreply.github.com>
2024-08-06 15:33:49 +0200

    maj vers 3.2.0

--- install/maj_3.1.0.sh
+++ install/maj_3.1.0.sh
@@ -1,34 +1,14 @@
 !/usr/bin/bash
 
-echo "----------------------------------------------------------------------"
-echo "-----------Mise à jour vers la version 3.1.1  ------------------------"
-echo "----------------------------------------------------------------------"
-echo "appuyer sur ENTER pour continuer"
-read
-mkdir tmp
-wget -O tmp/maj.zip https://github.com/mgrafr/monitor/archive/refs/heads/miseajour.zip
-unzip tmp/maj.zip
-cp -RTu --update monitor-miseajour/include include
-cp -RTu --update monitor-miseajour/css  css
-cp -RTu --update monitor-miseajour/js  js
-cp -u --update monitor-miseajour/fonctions.php  fonctions.php
-#
-sed -i "s/*************************Pour Domoticz/ modules complementaires dz/g" /var/www/html/monitor/admin/config.php
-sed -i "s/URLDOMOTIC.'modules_lua\/string_t/'admin\/connect\/string_t/g" /var/www/html/monitor/admin/config.php
-sed -i "s/URLDOMOTIC.'modules_lua\/c/'admin\/connect\/c/g" /var/www/html/monitor/admin/config.php
-sed -i "s/URLDOMOTIC.'modules_lua\/string_modect.lua/'admin\/connect\/string_modect.json/g" /var/www/html/monitor/admin/config.php
-#
-rm -R tmp
-rm .version
-cp monitor-miseajour/.version .version
-rm -R monitor-miseajour
-echo "-----------------------------------------------------------------------"
-echo "-----------Mises à jour vers la version 3.1.1  terminées---- ----------"
-echo "-----------------------------------------------------------------------"
-echo "----------jpgraph peut être mis à jour vers la version 4.4.2-----------"
-echo "       pour cela téléccharger le référentiel                           "
-echo "     extraire le dossier jgraph pour remplacer la version 4.4.1        "
-echo "-----------------------------------------------------------------------"
-echo "-----------------------------------------------------------------------"
-echo "          le fichier python upload.py pour DZ a été modifié            "
-echo "-----------------------------------------------------------------------"
+echo "-**********************************************************************-"
+echo "------------------------------------------------------------------------"
+echo "-----------Mise à jour vers la version 3.2.0  --------------------------"
+echo " cette version ne peut être mise à jour automatiquemnt,                 " 
+echo " aussi la configuration doit être indiquée dans le nouveau fichier      " 
+echo " en s’inspirant du précédent.                                           "
+echo " Dans la base de données de monitor seul le champ Actif doit être       "
+echo " si besoin mis à jour en tenant compte des infos indiquées dans         "
+echo " Read the Docs:                                                         "
+echo " https://monitor-domotique.readthedocs.io/fr/latest/guides/update.html  "
+echo "------------------------------------------------------------------------"
+echo "------------------------------------------------------------------------"
