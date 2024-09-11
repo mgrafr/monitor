af985da2713983fd350202b9b1f05067f2e34db2
domo-site <93765654+mgrafr@users.noreply.github.com>
2024-09-11 17:42:44 +0200

    Add files via upload

--- include/iobroker.php
+++ include/iobroker.php
@@ -1,10 +1,10 @@
 <?php
 //session_start();
 $domaine=$_SESSION["domaine"];
-if ($domaine==URLMONITOR) {$lien_iobroker=URLIOB.'/webui/';$lien_iobroker1=URLIOB;}//header("Access-Control-Allow-Origin: 'https://monitoring.la-truffiere.ovh'");
-if ($domaine==IPMONITOR) {$lien_iobroker='http://'.$IP_iob.':'.$port_webui_iob;$lien_iobroker1='http://'.$IP_iob.':'.substr($port_webui_iob, 0, 4);}
-
-
+if ($domaine==URLMONITOR) {$lien_iobroker=URLIOB[1].'/vis-2/index.html#Page1';$lien_iobroker1=URLIOB[0];
+						   $lien_iobroker2=URLIOB[1];}
+if ($domaine==IPMONITOR) {$lien_iobroker='http://'.$IP_iob.':'.$port_webui_iob;
+						  $lien_iobroker1='http://'.$IP_iob.':'.substr($port_webui_iob, 0, 4);}
 
 ?>
 <!-- section io.broker start -->
@@ -12,12 +12,16 @@ if ($domaine==IPMONITOR) {$lien_iobroker='http://'.$IP_iob.':'.$port_webui_iob;$
 <div id="iobroker" >
 	<div class="container">
 		<div class="col-md-12">
-	  <h1 id="about" class="title" style="position:relative;top:10px;">IO BROKER : <span style="color:blue">io.broker</span></h1>
-			<p><a href="<?php echo $lien_iobroker1;?>" target="_blank" title="io.broker"><?php echo $lien_iobroker1;?></a></p>
-			<p><a href="<?php echo $lien_iobroker.'runtime.html';?>" target="_blank" title="io.broker"><?php echo $lien_iobroker.'runtime.html';?></a></p> 
-	  <iframe id="iobrokerapp" src="<?php echo $lien_iobroker.'runtime.html';?>" frameborder="0" ></iframe>
-		       
-		<div class="modal" id="infos"></div>
+	  <h1 id="about" class="title" style="position:relative;top:10px;">IO BROKER :
+		  <span style="color:blue">io.broker</span></h1>
+			<p><a href="<?php echo $lien_iobroker1;?>" target="_blank" title="io.broker">
+				<?php echo $lien_iobroker1;?></a><br>
+			<a href="<?php echo $lien_iobroker2;?>" target="_blank" title="io.broker">
+				<?php echo $lien_iobroker2;?></a><br>	
+			<a href="<?php echo $lien_iobroker;?>" target="_blank" title="io.broker">
+				<?php echo $lien_iobroker;?></a></p> 
+<iframe id="iobrokerapp" src="<?php echo $lien_iobroker;?>" frameborder="0" ></iframe>
+	
 </div>
 	</div>
 		</div> 	
