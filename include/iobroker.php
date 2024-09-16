2e0e582adadab6070f49c913006578ad41a239d2
domo-site <93765654+mgrafr@users.noreply.github.com>
2024-09-16 17:46:24 +0200

    Add files via upload

--- include/iobroker.php
+++ include/iobroker.php
@@ -1,7 +1,7 @@
 <?php
 //session_start();
 $domaine=$_SESSION["domaine"];
-if ($domaine==URLMONITOR) {$lien_iobroker=URLIOB[1].'/vis-2/index.html#Page1';$lien_iobroker1=URLIOB[0];
+if ($domaine==URLMONITOR) {$lien_iobroker=URLIOB[1].'/vis-2/index.html';$lien_iobroker1=URLIOB[0];
 						   $lien_iobroker2=URLIOB[1];}
 if ($domaine==IPMONITOR) {$lien_iobroker='http://'.$IP_iob.':'.$port_webui_iob;
 						  $lien_iobroker1='http://'.$IP_iob.':'.substr($port_webui_iob, 0, 4);}
@@ -14,6 +14,7 @@ if ($domaine==IPMONITOR) {$lien_iobroker='http://'.$IP_iob.':'.$port_webui_iob;
 		<div class="col-md-12">
 	  <h1 id="about" class="title" style="position:relative;top:10px;">IO BROKER :
 		  <span style="color:blue">io.broker</span></h1>
+			<input type="button" value="refresh" onclick="refreshIframe()"/>
 			<p><a href="<?php echo $lien_iobroker1;?>" target="_blank" title="io.broker">
 				<?php echo $lien_iobroker1;?></a><br>
 			<a href="<?php echo $lien_iobroker2;?>" target="_blank" title="io.broker">
@@ -25,6 +26,11 @@ if ($domaine==IPMONITOR) {$lien_iobroker='http://'.$IP_iob.':'.$port_webui_iob;
 </div>
 	</div>
 		</div> 	
-
+<script type="text/javascript">
+	function refreshIframe(){
+    var iframe = document.getElementById('iobrokerapp'); 
+    iframe.src='http://192.168.1.162:8082/vis-2/index.html';
+    }
+</script>
 <!-- section io.broker fin-->
 
