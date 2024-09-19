456bc03647e32daa9149ffb14ac53cc2906798a0
domo-site <93765654+mgrafr@users.noreply.github.com>
2024-09-19 17:31:49 +0200

    Add files via upload

--- include/accueil.php
+++ include/accueil.php
@@ -21,7 +21,7 @@
 <?php $domaine=$_SESSION["domaine"];
 if ($domaine==URLMONITOR) $lien_img="/";
 if ($domaine==IPMONITOR) $lien_img="/monitor/";
-if (ON_SOS==true) echo '<img id="SOS" class="sos" src="'.$lien_img.'images/sos.svg" width="40" height="auto" alt=""/>';?>
+?>
 <div class="confirm pression_chaud"><a href="#" id="annul_pression" rel="1002" title="Annulation de l'\alerte pression"><img id="pression_chaud" src=""/></a></div>
 <div class="confirm pilule"><a href="#" id="annul_pilule" rel="1001" title="Annulation de l'\alerte pilule michel"><img id="pilule" src=""/></a></div>
 <div class="confirm"><a href="#" id="annul_fosse" rel="1011" title="Annulation de l'\alerte fosse septique"><img id="fosse" class="fosse_septique" src=""/></a></div>
@@ -30,6 +30,7 @@ if (ON_SOS==true) echo '<img id="SOS" class="sos" src="'.$lien_img.'images/sos.s
 	<div id="pluie" ><img id="pl" src="" alt="pluie" /></div><div id="txt_pluie"></div>  </div>
 <div class="aff_anni" ><img id="aff_anni" src="" alt="anni" /><div id="prenom"></div></div>
 <div class="aff_al" ><img id="alarme_nuit" src="images/alarme_auto.svg" alt="alarme" /></div>
+<?php if (ON_SOS==true) echo '<div class="sos" ><img id="SOS" src="images/sos.svg" width="40" alt="" /></div>';?>
 <div class="aff_bat" ><img id="batterie" src="images/batterie_faible.svg" alt="batterie" /></div>
 <div class="confirm ping_rasp" ><a href="#" id="annul_ping" rel="1004" title="effacement alerte pi"><img id="ping_rasp" style="width:40px;height:40px" src="" alt="ping" /></a></div>  
 <div class="confirm bl" ><a href="#" id="confirm-box" rel="1018" title="courrier récupéré"><img id="bl" src="images/boite_lettres.svg" alt="boite_lettres" /></a></div>
