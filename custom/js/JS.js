66a0a890f71ab9fce9821bf16c59406114801077
domo-site <93765654+mgrafr@users.noreply.github.com>
2024-10-15 16:23:10 +0200

    Add files via upload

--- custom/js/JS.js
+++ custom/js/JS.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 
-function custom_js(){
+function custom_js(custom){
 if (typeof custom != 'undefined' & custom==1) {worx=pp[200].value;maj_worx(pp[200].Name,pp[200].Data);}	
 }
 
