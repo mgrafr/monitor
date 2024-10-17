16191399fed0b373d9614f6ad26961d0bbbbca83
domo-site <93765654+mgrafr@users.noreply.github.com>
2024-10-17 18:40:53 +0200

    Add files via upload

--- include/backup_bd.php
+++ include/backup_bd.php
@@ -1,16 +1,6 @@
 <?php
 require_once (MONCONFIG);
 
-if (IsDir_or_CreateIt(BACKUP_DB)==true){
-// SERVEUR SQL connexion
-$conn = new mysqli(SERVEUR,UTILISATEUR,MOTDEPASSE,DBASE);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error . "<br>");
-}
-// Génération du fichier de sauvegarde dans le répertoire DB_Backup
-shell_exec(' mysqldump  --databases '.DBASE.' --user='.UTILISATEUR.' --password='.MOTDEPASSE.' > '.BACKUP_DB.'/'.DBASE.'.sql');echo 'SUCCES:';
-
-}
 function IsDir_or_CreateIt($path) {
       if(is_dir($path)) {
         return true;
@@ -23,4 +13,34 @@ function IsDir_or_CreateIt($path) {
       }
     }
 
+if (IsDir_or_CreateIt(BACKUP_DB)==true){
+// SERVEUR SQL connexion
+$conn = new mysqli(SERVEUR,UTILISATEUR,MOTDEPASSE,DBASE);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error . "<br>");
+}
+	echo $choix;
+switch($choix){
+	case 14:	
+shell_exec(' mysqldump  --databases '.DBASE.' --user='.UTILISATEUR.' --password='.MOTDEPASSE.' > '.BACKUP_DB.'/'.DBASE.'.sql');echo 'SUCCES:';
+break;
+	case 27:
+$query=	' mysql --databases monit --user='.UTILISATEUR.' --password='.MOTDEPASSE.' < '.$_POST['textfield'].';';		
+echo $query;
+echo'<form name="form_backup" method="post" action="'.$_SERVER['PHP_SELF'].'"
+  <label>Chemin du Backup :<br><input type="text" name="textfield" id="textfield" style="width:400px;margin-left:10px;" value="/var/www/html/monitor/DB_Backup/monitor.sql" >
+  <input type="submit" name="button" id="button" value="Submit"></label>
+  </form>';		
+if (!empty($_POST['textfield'])) {
+shell_exec($query);  
+echo "Restauration réussie";		}
+    
+	
+break;	
+	default:
+echo "break"	;
+}
+	
+}
+else echo "erreur";
 ?>
