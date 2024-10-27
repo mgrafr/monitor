1232f2ee10b745040672f014e3500daa7e8df4f8
domo-site <93765654+mgrafr@users.noreply.github.com>
2024-10-27 16:26:46 +0100

    Update restore.sh

--- install/restore.sh
+++ install/restore.sh
@@ -1,14 +1,3 @@
-
-#!/usr/bin/bash
-#sur le serveur monitor actuel
-pip list --format=json > /var/www/monitor/admin/connect/mod.json
-ufw status > /var/www/monitor/admin/connect/ufw.txt
-xxx=$(hostname -I)
-echo $xxx | cut -d ' ' -f 1 > /var/www/monitor/admin/connect/ip.txt
-echo "pour letsencrypt remplacement port 80 par 81"
-sed  -i "s/80/81/g" /home/$mdir_maj/etc/nginx/conf.d/monitor.conf
-
-
 #!/usr/bin/bash
 #sur le nouveau serveur monitor 
 mdir_maj=$(whiptail --title "Création d'un réperoire de travail " --inputbox "veuillez entrer le du répertoire \n\n Entrer répertoire" 10 60 3>&1 1>&2 2>&3)
