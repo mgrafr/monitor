a235ce8e3ee05661e629f2eaf4e7c859b09b646e
domo-site <93765654+mgrafr@users.noreply.github.com>
2024-11-03 09:01:55 +0100

    Add files via upload

--- install/restore.sh
+++ install/restore.sh
@@ -10,6 +10,7 @@ echo "Par défaut, utlisateur enregistré : "$mdir_maj
 fi 
 mkdir -p /home/$mdir_maj/monitor/{admin,custom,DB_Backup}
 mkdir -p /home/$mdir_maj/etc/{letsencrypt,ssl,nginx{conf.d,ssl},cron.d,}
+mkdir /home/$mdir_maj/root/.ssh
 read ip3 < /var/www/monitor/admin/connect/ip.txt
 echo "adresse IP:" $ip3
 domaine=`grep $choix'domaine=' /var/www/monitor/admin/connect/connect.py | cut -f 2 -d '='`
@@ -32,6 +33,15 @@ lets=$(whiptail --title "Certificat Letsencrypt" --radiolist \
 "Comment voulez vous mettre à jour monitor ?\n avec les certificat SSL enregistrés\n sans les certificats SSL " 15 60 4 \
 "Avec les certificats déjà enregistrés" "par defaut " ON \
 "Sans certificats" "voir la doc" OFF 3>&1 1>&2 2>&3)
+if [ $exitstatus = 0 ]; then
+   echo "Vous avez choisi  : $lets"
+else
+echo "Vous avez annulé  "
+fi
+lets=$(whiptail --title "clé SSH" --radiolist \
+"Possédez-vous une ou plusieurs clés SSH ? ?\n voulez vous les copier ?" 15 60 4 \
+"Copier les clés déjà enregistrés" "par defaut " ON \
+"Ne pas copier les clés " "voir la doc" OFF  3>&1 1>&2 2>&3)
 if [ $exitstatus = 0 ]; then
    echo "Vous avez choisi  : $lets"
 else
@@ -69,6 +79,14 @@ get /etc/cron.d/*
 exit
 EOF
 fi
+if [ "lets" = "Copier les clés déjà enregistrés" ]
+then
+sftp $mdir_maj@192.168.1.9<<EOF
+lcd /etc/root/.ssh
+get /root/.ssl/*
+exit
+EOF
+fi
 cd /home/$mdir_maj/etc/letsencrypt
 rm -R live
 mkdir live
@@ -90,10 +108,12 @@ cp  /home/$mdir_maj/etc/nginx/conf.d/* /etc/nginx/conf.d/
 mkdir /etc/nginx/ssl
 cp  /home/$mdir_maj/etc/nginx/ssl/* /etc/nginx/ssl/
 chmod -R 777 /etc/letsencrypt
-cp -R etc/letsencrypt/* /etc/letsencrypt/
-cp etc/cron.d/* /etc/cron.d
+cp -R /home/$mdir_maj/etc/letsencrypt/* /etc/letsencrypt/
 chmod -R 777 /etc/cron.d
+cp /home/$mdir_maj/etc/cron.d/* /etc/cron.d
 systemctl reload nginx
+chmod -R 777 /root/.ssl
+cp /home/$mdir_maj/root/.ssh/* /root/.ssh
 certbot update_symlinks
 certbot renew --dry-run
 
