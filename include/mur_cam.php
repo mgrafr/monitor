0f54377ad19397a6d9437e0916c756956843ef01
domo-site <93765654+mgrafr@users.noreply.github.com>
2024-12-24 09:40:18 +0100

    ajout Frigate

--- include/mur_cam.php
+++ include/mur_cam.php
@@ -1,19 +1,21 @@
 <!-- ================ -->
-		<div id="murcam" class="mur_cam" >
-			<div class="container">
+<div id="murcam" class="mur_cam" >
+  <div class="container">
 		<div class="col-md-12 colmur">
 	  <h4 id="titre_mur" class="title text-center">Mur<span> des caméras</span></h4>
-	  <?php echo (NOMMUR . " - " . NBCAM ); ?>
-			<br><input id="onoffmur" type="checkbox" data-toggle="toggle" ><div id="aqw">Vidéo inactive</div>
-</div>
-							</div>
-			<div id="message1" class="space">cliquer sur une image pour activer le zoom video de l'image</div><table class="cam">
-               <tbody>
+	  <?php 
+    if (SRC_MUR != "fr") {echo ' '.NOMMUR.' - '.NBCAM.'<br><input id="onoffmur" type="checkbox" data-toggle="toggle" ><div id="aqw">Vidéo inactive'; } 
+    else {echo  " ".NOMMUR."<br>";}
+	 ?> 
+	  </div>
+  </div>
+
 <?php
-$scale=100;
-$i=1;$j=1;
-				   
-while ($i <= NBCAM) {
+if (SRC_MUR != "fr") {echo '<div id="message1" class="space">cliquer sur une image pour activer le zoom video de l\'image</div><table class="cam">
+  <tbody>';
+  $scale=100;
+  $i=1;$j=1;
+  while ($i <= NBCAM) {
 	if (SRC_MUR=="mo") {$src_img='include/mur_cameras1.php?id_zm='.$i.'&x=';}
 	if (SRC_MUR=="zm") {$src_img='include/mur_cameras.php?idx='.$i.'&url='.ZMURL.'&x=';}	
 	$camImgId="cam".$i;
@@ -22,14 +24,14 @@ while ($i <= NBCAM) {
 	<img id="'.$camImgId.'" src="'.$src_img.'" rel="'.$i.'" class="dimcam" alt=""/></td>';
 	if (($j==2) || ($i==NBCAM)){ echo "</tr>";$j=0;}
   $i++;$j++;}				
-
-?>
-</tbody>
-      </table>		
-		  
-             
-				</div>
-				</div>
+  echo '</tbody></table>';}
+if (SRC_MUR=="fr") {$domaine=$_SESSION["domaine"];
+  if ($domaine==URLMONITOR) $lien_frigate=URL_FRIGATE;
+  if ($domaine==IPMONITOR) $lien_frigate="http://".IP_FRIGATE;
+  echo '<iframe id="frigate" src="'.$lien_frigate.'" frameborder="0" ></iframe>';}
+?>		
+</div>
+</div>
 <!-- Creates the bootstrap modal where the image will appear -->
 <div class="modal fade" id="imagemodal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
   <div class="modal-dialog">
