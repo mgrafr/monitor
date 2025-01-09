f3224438c217ed46a943f66bc59055678993ad1f
domo-site <93765654+mgrafr@users.noreply.github.com>
2025-01-09 18:07:16 +0100

    Add files via upload

---
+++ share/scripts_dz/py/fr_mqtt.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3.9
+#-*- coding: utf-8 -*-
+import sys
+from connect import ip_mqtt
+import paho.mqtt.client as mqtt #import the client1
+broker_address=ip_mqtt
+topic = str(sys.argv[1])
+payload = str(sys.argv[2])
+#broker_address="iot.eclipse.org" #use external broker
+client = mqtt.Client("P1") #create new instance
+client.username_pw_set(username='michel',password='Idem4546')
+client.connect(broker_address) #connect to broker
+client.publish(topic,payload) #publish
