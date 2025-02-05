16a0dd8d347e6a6c77c1f20a3981397d1d1a3551
domo-site <93765654+mgrafr@users.noreply.github.com>
2025-02-05 16:56:30 +0100

    Add files via upload

--- install/sauvegarde_maj.sh
+++ install/sauvegarde_maj.sh
@@ -5,4 +5,4 @@ ufw status > /var/www/monitor/admin/connect/ufw.txt
 xxx=$(hostname -I)
 echo $xxx | cut -d ' ' -f 1 > /var/www/monitor/admin/connect/ip.txt
 echo "pour letsencrypt remplacement port 80 par 81"
-sed  -i "s/80/81/g" /home/$mdir_maj/etc/nginx/conf.d/monitor.conf
+sed  -i "s/80/81/g" /etc/nginx/conf.d/monitor.conf
