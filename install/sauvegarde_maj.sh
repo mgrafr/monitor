919e7d00fa1dfd32401760eb94c7e8922b8789d9
domo-site <93765654+mgrafr@users.noreply.github.com>
2025-02-07 15:09:14 +0100

    Add files via upload

--- install/sauvegarde_maj.sh
+++ install/sauvegarde_maj.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/bash
 #sur le serveur monitor actuel
+chmod -R 777 /var/www/monitor/DB_Backup
+nom_bd=$(whiptail --title "Bases de donnees a sauvegarder" --inputbox "Veuiller entrer les noms separes par un espace" 10 60 3>&1 1>&2 2>&3)
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+echo "Base(s) de donnees : "$nom_bd
+else
+nom_bd=monitor
+echo "Par defaut, base de donnees : "$nom_bd
+fi
+mysqldump -u root -p --databases $nom_bd  | gzip  > /var/www/monitor/DB_Backup/dump.sql.gz
 pip list --format=json > /var/www/monitor/admin/connect/mod.json
 ufw status > /var/www/monitor/admin/connect/ufw.txt
 xxx=$(hostname -I)
