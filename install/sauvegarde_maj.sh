bcbbc51d14d12095e13bc73ab933b95dff059c6c
domo-site <93765654+mgrafr@users.noreply.github.com>
2025-02-09 14:50:09 +0100

    Update sauvegarde_maj.sh

--- install/sauvegarde_maj.sh
+++ install/sauvegarde_maj.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 #sur le serveur monitor actuel
 chmod -R 777 /var/www/monitor/DB_Backup
+chmod -R 644 /etc/letsencrypt/archive/*
 nom_bd=$(whiptail --title "Bases de donnees a sauvegarder" --inputbox "Veuiller entrer les noms separes par un espace" 10 60 3>&1 1>&2 2>&3)
 exitstatus=$?
 if [ $exitstatus = 0 ]; then
