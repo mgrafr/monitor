77cff0d20d4cabffd8fba508435d284e73203b7d
domo-site <93765654+mgrafr@users.noreply.github.com>
2025-02-10 15:09:09 +0100

    Update sauvegarde_maj.sh

--- install/sauvegarde_maj.sh
+++ install/sauvegarde_maj.sh
@@ -17,3 +17,5 @@ xxx=$(hostname -I)
 echo $xxx | cut -d ' ' -f 1 > /var/www/monitor/admin/connect/ip.txt
 echo "pour letsencrypt remplacement port 80 par 81"
 sed  -i "s/80/81/g" /etc/nginx/conf.d/monitor.conf
+cd /etc/systemd/system
+find . -type f -prune > /www/monitor/c.txt
