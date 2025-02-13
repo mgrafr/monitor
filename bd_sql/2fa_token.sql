82a4e2499c953fdf7a4302c7dcb82bee50032a4c
domo-site <93765654+mgrafr@users.noreply.github.com>
2025-02-13 21:17:19 +0100

    Add files via upload

--- bd_sql/2fa_token.sql
+++ bd_sql/2fa_token.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.2
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : mer. 05 fév. 2025 à 09:02
+-- Généré le : jeu. 13 fév. 2025 à 20:15
 -- Version du serveur : 10.11.6-MariaDB-0+deb12u1
 -- Version de PHP : 8.3.16
 
@@ -31,15 +31,17 @@ CREATE TABLE `2fa_token` (
   `num` int(3) NOT NULL,
   `user_id` varchar(20) NOT NULL,
   `token` varchar(20) NOT NULL,
-  `sms` int(1) NOT NULL DEFAULT 0
+  `sms` int(1) NOT NULL DEFAULT 0,
+  `user_free` varchar(10) NOT NULL,
+  `pass_free` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
 --
 -- Déchargement des données de la table `2fa_token`
 --
 
-INSERT INTO `2fa_token` (`num`, `user_id`, `token`, `sms`) VALUES
-(1, 'user', '', 0);
+INSERT INTO `2fa_token` (`num`, `user_id`, `token`, `sms`, `user_free`, `pass_free`) VALUES
+(1, '', '', 2, '', '');
 
 --
 -- Index pour les tables déchargées
