dd295edc2085a386a210ad3756e1d66e484a5d67
domo-site <93765654+mgrafr@users.noreply.github.com>
2025-02-26 10:35:57 +0100

    Add files via upload

---
+++ OTP/verify_otp.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once("PragmaRX/Google2FA/Google2FA.php");
+//$conf=$_SESSION["config"];
+//require("../".$conf);
+use PragmaRX\Google2FA\Google2FA;
+
+
+// Start Session
+if(!session_id())
+{
+    session_start();
+}
+
+// Validate incoming request
+
+if(empty($_POST['otp']) || empty($_SESSION['user']['google2fa_secret'])) {
+    die(json_encode(['result' => false]));
+}
+
+// Initialize
+$googleOTP = new Google2FA();
+
+// Retrieve One Time Password from payload
+$otp = $_POST['otp'];
+$_SESSION['otp'] = $otp; 
+// Verify provided OTP
+$isValid = $googleOTP->verifyKey($_SESSION['user']['google2fa_secret'], $otp);
+//maj sql
+if ($isValid ===true && $_POST['maj_bd'] == true){
+$data= [
+    'command' => '15',
+    'token' => $_SESSION['user']['google2fa_secret'] ];
+mysql_app($data);
+}
+// Generate and print JSON response
+$retour=json_encode([
+    'provided_otp' => $otp, 
+    'result' => $isValid
+ ]);
+echo $retour;
+?>
