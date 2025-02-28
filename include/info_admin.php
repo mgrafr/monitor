0d72589149990a3fff53679cc5fdf09404e36220
domo-site <93765654+mgrafr@users.noreply.github.com>
2025-02-28 11:05:17 +0100

    Add files via upload

--- include/info_admin.php
+++ include/info_admin.php
@@ -3,9 +3,9 @@ $info_admin=array();
 $info_admin = [
 	11 => '<p>Pour afficher la liste des modules Python , <br><img src="images/info14.webp" style="width:400px;height="auto";margin: 0 10px;" alt=""/></p>',
 	10 => '<p>Pour afficher la liste des variales dans SQL , <br><img src="images/info12.webp" style="width:400px;height="auto";margin: 0 10px;" alt=""/></p>',
-	9 => '<p>Pour mettre à jour automatiquement la base de données SQL , table messages:<br>pour DZ,HA ou autres app, 1 message=1 ID<br><img src="images/info11.webp" style="width:281px;height="auto";margin: 0 10px;" alt=""/></p>',
-	8 => '<p>- Version monitor:  '.$_SESSION["version"].'<br>- Version PHP:      8.2<br>- Version Jpgraph:  4.4.1<br>
-	        - Version Bootstrap: 4.5.2</p>',
+	9 => '<p>Pour mettre à jour automatiquement la base de données SQL , table messages:<br>pour DZ,HA,IOB ou autres app, 1 message=1 ID<br><img src="images/info11.webp" style="width:281px;height="auto";margin: 0 10px;" alt=""/></p>',
+	8 => '<p>- Version monitor:  '.$_SESSION["version"].'<br>- Version PHP:      8.3<br>- Version Jpgraph:  4.4.1<br>
+	        - Version Bootstrap: 4.5.2<br>- Version Mariadb Server: 10.11.6</p>',
 	7 => '<p>Pour mettre à jour automatiquement la base de données SQL à partir :<br>des données de Domoticz ou HA et de Monitor<br><img src="images/info10.webp" style="width:281px;height="auto";margin: 0 10px;" alt=""/></p>',
 	0 => '<p>Pour mettre à jour automatiquement la base de données SQL à partir :<br>des données de Domoticz ou HA et de Monitor<br><img src="images/info1.webp" style="width:281px;height="auto";margin: 0 10px;" alt=""/></p>',
     1 => '<p>Pour configurer le programme :<br>- indiquer les IP,les Mots de passe,...<br>- Choisir le nom des variables utilisées,..<img src="images/info2.webp" style="width:300px;height="auto";margin: 0 10px;" alt=""/></p>',
