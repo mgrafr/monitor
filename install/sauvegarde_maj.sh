10c270ba3a20dbe5216760c20a3d5c394eff1979
domo-site <93765654+mgrafr@users.noreply.github.com>
2025-05-23 09:41:52 +0200

    Add files via upload

--- install/sauvegarde_maj.sh
+++ install/sauvegarde_maj.sh
@@ -15,9 +15,22 @@ pip list --format=json > /var/www/monitor/admin/connect/mod.json
 ufw status > /var/www/monitor/admin/connect/ufw.txt
 xxx=$(hostname -I)
 echo $xxx | cut -d ' ' -f 1 > /var/www/monitor/admin/connect/ip.txt
+
+choix_https=$(whiptail --title "accès distant" --radiolist \
+"avez vous une adresse distante HTTPS ?" 15 60 4 \
+"Pas de https  " "par defaut " ON \
+"accès https" "voir la doc" OFF 3>&1 1>&2 2>&3)
+if [ $exitstatus = 0 ]; then
+   echo "Vous avez choisi  : $choix_https"
+else
+echo "Vous avez annulé  "
+fi
+if [ "$choix_https" = "accès https" ]
+then
 echo "pour letsencrypt remplacement port 443 par 444"
 sed  -i "s/443/444/g" /etc/nginx/conf.d/monitor.conf
 systemctl restart nginx
+fi
 cd /etc/systemd/system
 find . -type f -prune > /www/monitor/c.txt
 echo "------------------------------------------------------------------"
