0be511701ee612b70cc2d4020d482ab2ea32dcdf
domo-site <93765654+mgrafr@users.noreply.github.com>
2025-05-27 23:05:25 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ echo '<!DOCTYPE html><html><body style="background-color: cornsilk;">';
 $rep="/"; $domaine=$_SERVER['HTTP_HOST'];$port=$_SERVER['SERVER_PORT'];
 $seg = $_SERVER['REQUEST_URI'];
 $reg=str_replace('/monitor/','',$seg);
+$reg=str_replace('/','',$reg);
 $reg=str_replace('?','',$reg);
 $_SESSION["conf"]=$reg;
 if (substr($domaine, 0, 7)=="192.168") $rep="/monitor/";
