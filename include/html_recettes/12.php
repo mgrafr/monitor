489ff6cfed3efb2d6e036c307828240acdf857aa
domo-site <93765654+mgrafr@users.noreply.github.com>
2025-06-06 11:34:06 +0200

    Add files via upload

---
+++ include/html_recettes/12.php
@@ -0,0 +1,26 @@
+<style>.txt1_cuisine{
+margin-left: 10px;
+    font-size: 18px;
+    font-family: auto;
+font-style: oblique;}</style>
+
+<p ><strong>Quiche maison</strong>&nbsp;<em></em><br>
+ingrédients pour 3 à 4 Personnes <p>	
+<div class="txt1_cuisine" style="margin-left:10px" ><img src="$$images/pate_feuilletée.webp" style="vertical-align: middle; width:60px;heght:auto" alt=""/>
+  &nbsp;&nbsp;&nbsp;<span class="txt1_cuisine">230 g pate feuilletée</span></div>
+<div class="txt1_cuisine" style="margin-top: 5px;margin-left:10px"><img src="$$images/lardons.webp" style="vertical-align: middle; width:60px;heght:auto" alt=""/>
+  &nbsp;&nbsp;&nbsp;&nbsp;100-200g lardons</div>
+<div class="txt1_cuisine" style="margin-left:10px"><img src="$$images/creme-fraiche.webp" style="vertical-align: middle;width:60px;heght:auto" alt=""/>
+  &nbsp;&nbsp;&nbsp;20 cl crèmefraiche</div>
+<div class="txt1_cuisine" style="margin-left:10px"><img src="$$images/lait.webp" style="vertical-align: middle;width:60px;heght:auto" alt=""/>
+	&nbsp;&nbsp;&nbsp;20 cl lait</div>
+<div class="txt1_cuisine" style="margin-left:10px"><img src="$$images/oeufs.webp" style="vertical-align: middle;width:60px;heght:auto" alt=""/>
+	&nbsp;&nbsp;&nbsp;3 oeufs</div>
+<div class="txt1_cuisine" style="margin-left:10px"><img src="$$images/gruyere.webp" style="vertical-align: middle;width:60px;heght:auto" alt=""/>
+	&nbsp;&nbsp;&nbsp;140 g gruyère rapé</div>
+<div class="txt1_cuisine" style="margin-left:10px"><img src="$$images/beurre.webp" style="vertical-align: middle;width:60px;heght:auto" alt=""/><img src="$$images/sel_poivre.webp" style="vertical-align: middle;width:60px;heght:auto" alt=""/>
+	&nbsp;&nbsp;&nbsp;beure, sel,poivre</div>
+<p> <strong>Préparation :</strong><br>
+	<strong>Suggestion :</strong> ajouter des champignon<img src="$$images/champignons.webp" style="vertical-align: middle;width:60px;heght:auto" alt=""/><br>
+	<strong>Cuisson :</strong><br>
+	Mettre au four à 200° pendant 35 minutes.</p>
---
+++ include/html_recettes/13.php
@@ -0,0 +1,26 @@
+<style>.txt1_cuisine{
+margin-left: 10px;
+    font-size: 18px;
+    font-family: auto;
+font-style: oblique;}</style>
+
+<p ><strong>Lasagnes patate douce</strong>&nbsp;<em></em><br>
+ingrédients pour 3 à 4 Personnes <p>	
+<div class="txt1_cuisine" style="margin-left:10px" ><img src="$$images/patate_douce.webp" style="vertical-align: middle; width:60px;heght:auto" alt=""/>
+  &nbsp;&nbsp;&nbsp;<span class="txt1_cuisine">2 patates douces</span></div>
+<div class="txt1_cuisine" style="margin-top: 5px;margin-left:10px"><img src="$$images/oignons.webp" style="vertical-align: middle; width:60px;heght:auto" alt=""/>
+  &nbsp;&nbsp;&nbsp;&nbsp;1 oignon</div>
+<div class="txt1_cuisine" style="margin-left:10px"><img src="$$images/poivrons.webp" style="vertical-align: middle;width:60px;heght:auto" alt=""/>
+  &nbsp;&nbsp;&nbsp;1 poivron rouge, 1 poivron jaune</div>
+<div class="txt1_cuisine" style="margin-left:10px"><img src="$$images/courgettes.webp" style="vertical-align: middle;width:60px;heght:auto" alt=""/>
+	&nbsp;&nbsp;&nbsp;1 courgette</div>
+<div class="txt1_cuisine" style="margin-left:10px"><img src="$$images/carottes.webp" style="vertical-align: middle;width:60px;heght:auto" alt=""/>
+	&nbsp;&nbsp;&nbsp;2 carottes</div>
+<div class="txt1_cuisine" style="margin-left:10px"><img src="$$images/sauce_tomate.webp" style="vertical-align: middle;width:60px;heght:auto" alt=""/>
+	&nbsp;&nbsp;&nbsp;50 cl de sauce tomate</div>
+<div class="txt1_cuisine" style="margin-left:10px"><img src="$$images/parmesan.webp" style="vertical-align: middle;width:60px;heght:auto" alt=""/><img src="$$images/ail.webp" style="vertical-align: middle;width:60px;heght:auto" alt=""/><img src="$$images/persil.webp" style="vertical-align: middle;width:60px;heght:auto" alt=""/>
+	&nbsp;&nbsp;&nbsp;parmesan(4 cs),ail,persil frais</div>
+<p> <strong>Préparation :</strong><br>- Pelez les patates douces puis découpez-les en fines tranches à l’aide d’une mandoline.<br>
+	- Épluchez l’oignon et émincez-le. Rincez les courgettes, les poivrons et les carottes. <br>- Épluchez-les puis détaillez-les en petits dés. Pelez l’ail et écrasez-la.<br>- Dans une sauteuse, faites chauffer l’huile d’olive pour y faire revenir l’oignon émincé pendant 5 minutes<br>- Ajoutez les dés de poivrons puis laissez cuire pendant 5 minutes. <br>- Incorporer les courgettes et les carottes, ainsi que la gousse d’ail écrasée. <br>- Salez et poivrez selon votre goût; faites revenir 5 minutes puis versez la sauce tomate. <br>- Faites mijoter pendant 20 minutes en remuant de temps en temps.<br>- Préchauffez votre four à 180°C. Dressez vos lasagnes fondantes.<br> - Déposez dans le fond de votre plat un gratin, un peu de préparation aux légumes.<br> - Installez ensuite les fines tranches de patates douces. Couvrez d’une nouvelle couche de légumes puis de patates douces.<br> - Répétez l’opération en terminant par le mélange de légumes. Saupoudrez de parmesan râpé.
+	<br><br>- Enfournez vos lasagnes fondantes pendant 40 minutes à 180°C. Dégustez aussitôt.
+	</p>
