81f00022ddadeb1bb351d8f87713c702441e2467
domo-site <93765654+mgrafr@users.noreply.github.com>
2025-07-07 21:35:48 +0200

    Add files via upload

---
+++ maj_dev.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+cd /www/monitor
+sed -i "s/.DOMAINE\" ));/DOMAINE\",/g" admin/config.php
+sed -i "s/iobweb.DOMAINE",\iobweb.DOMAINE", 2 => \"false\"));/g" admin/config.php
+#----------------------------------------
+mkdir tmp
+git clone https://github.com/mgrafr/monitor.git
+cp -u ajax.php /www/monitor/ajax.php
+cp -u fonctions.php /www/monitor/fonctions.php
+cp -u index_loc.php /www/monitor/index_loc.php
+cp -u -R include/* /www/monitor/include/
+cp -u css/* /www/monitor/css/
+cp -u js/* /www/monitor/js/
+cp -u api/* /www/monitor/api/
+cp -u images/* /www/monitor/images/
+cp -u -R install/* /www/monitor/install/
+cp -u -R bd_sql/* /www/monitor/bd_sql/
+cp -u -R share/* /www/monitor/share/
+cp -u .version /www/monitor/.version
