248aa89815a1fb637f7fcbee04cddbf910dcf5de
domo-site <93765654+mgrafr@users.noreply.github.com>
2025-07-16 21:04:15 +0200

    Add files via upload

--- share/scripts_dz/sh/pushover.sh
+++ share/scripts_dz/sh/pushover.sh
@@ -4,8 +4,9 @@ APP_TOKEN="axxxxxxxxxxxxxxxxxxxxxa"
 USER_TOKEN="uxxxxxxxxxxxxxxxxxxi"
 MESSAGE=$1 
 echo $1
-curl -s -F "token=$APP_TOKEN" \
-   -F "user=$USER_TOKEN" \
-   -F "title=$TITLE" \
-   -F "message=$MESSAGE" \
+curl -s \
+  --form-string  "token=$APP_TOKEN" \
+  --form-string  "user=$USER_TOKEN" \
+  --form-string  "title=$TITLE" \
+  --form-string  "message=$MESSAGE" \
    https://api.pushover.net/1/messages.json
