e4be823bbc6be12cf27c62cadd8ccc38d23c6ec9
domo-site <93765654+mgrafr@users.noreply.github.com>
2025-09-13 11:09:06 +0200

    Update update.sh

--- install/update.sh
+++ install/update.sh
@@ -5,7 +5,7 @@ version=$(head -n 1 .version)
 echo $version
 vermon=$(whiptail --title "version de monitor" --radiolist \
 "Vers quelle version voulez vous mettre à jour ?\n la version en développement\n ou la version LATEST " 15 60 4 \
-"Version 3.2.5" "par defaut " ON \
+"Version 4.0.0" "par defaut " ON \
 "Version en dev" "voir la doc" OFF 3>&1 1>&2 2>&3)
 exitstatus=$?
 # ------------------------------
@@ -19,21 +19,23 @@ echo $answer
 apt update
 apt install dos2unix
 apt install unzip
-if [ "$vermon" = "Version 3.2.5" ] && [ "$answer" -gt 322 ] && [ "$answer" -ne 325 ]; then
-    echo "mise a jour vers 3.2.5"
-    wget https://raw.githubusercontent.com/mgrafr/monitor/main/install/maj_325.sh
-    dos2unix maj_325.sh
-    chmod +x maj_325.sh 
-    ./maj_325.sh
- elif [ "$vermon" =  "Version 3.2.5" ] && [ "$answer" -eq 324 ]; then 
-    echo "mise a jour vers 3.2.5"
+if [ "$vermon" = "Version 400 ] && [ "$answer" -gt 323 ] && [ "$answer" -ne 400 ]; then
+    echo "mise a jour vers 4.0.0"
+    wget https://raw.githubusercontent.com/mgrafr/monitor/main/install/maj_400.sh
+    dos2unix maj_400.sh
+    chmod +x maj_400.sh 
+    ./maj_400.sh
+ elif [ "$vermon" =  "Version 4.0.0" ] && [ "$answer" -eq 324 ]; then 
+    echo "mise a jour vers 4.0.0"
     wget https://raw.githubusercontent.com/mgrafr/monitor/main/install/maj_324.sh
     dos2unix maj_324.sh
     chmod +x maj_324.sh 
     ./maj_324.sh
- elif [ "$vermon" =  "Version 3.2.5" ] && [ "$answer" -eq 325 ]; then  
+ elif [ "$vermon" =  "Version 4.0.0" ] && [ "$answer" -eq 400 ]; then  
      echo "deja a jour"
- elif [ "$vermon" = "Version en dev" ] && [ "$answer" -gt 323 ]; then  
+ elif [ "$vermon" = "Version en dev" ] && [ "$answer" -lt 324 ] && [ "$answer" -gt 322 ]; then  
+      echo "mettre à jour vers version 4.0.0" avant une mise à jour DEV     
+ elif [ "$vermon" = "Version en dev" ] && [ "$answer" -gt 324 ]; then  
       echo "mise à jour vers version dev"  
       wget https://raw.githubusercontent.com/mgrafr/monitor/main/install/maj_dev.sh
       dos2unix maj_dev.sh
