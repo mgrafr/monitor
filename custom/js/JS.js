7a2e758f19d0eb0d63522334bb6b48c86d940b58
domo-site <93765654+mgrafr@users.noreply.github.com>
2025-11-18 22:39:01 +0100

    Update JS.js

--- custom/js/JS.js
+++ custom/js/JS.js
@@ -1,9 +1,11 @@
 // JavaScript Document
-
+// exemple pour un robot tondeuse worx, appel de cette fonction dans footer.php
+// custom=1 est défini dans worx.php
 function custom_js(custom){
 
 if (typeof custom != 'undefined' & custom==1) {if (pp[200].Name !== undefined) {
     worx=pp[200].values;maj_worx(pp[200].Name,pp[200].Data);}	
 }}
+
 
 				
