d3967001dacd599e2e62dece4ee507158035dfb2
domo-site <93765654+mgrafr@users.noreply.github.com>
2025-11-28 22:50:22 +0100

    Update maj_dev.sh

--- install/maj_dev.sh
+++ install/maj_dev.sh
@@ -11,7 +11,6 @@ cp -u ajax.php /www/monitor/ajax.php
 cp -u fonctions.php /www/monitor/fonctions.php
 cp -u index_loc.php /www/monitor/index_loc.php
 cp -u -R include/* /www/monitor/include/
-fi
 file=/www/monitor/custom/php/accueil.php
 if [ -e $file ]; then mv /www/monitor/include/accueil.php /www/monitor/include/_accueil.bak; fi
 cp -u css/* /www/monitor/css/
@@ -23,3 +22,9 @@ cp -u -R bd_sql/* /www/monitor/bd_sql/
 cp -u -R share/* /www/monitor/share/
 cp -u .version /www/monitor/.version
 rm -R tmp
+result()(mysql --user="root" --password="Idem4546" --database="monitor" --execute= -e "SHOW COLUMNS FROM dispositifs LIKE 'materiel';") 
+res=${result} 
+if [[ ${res:0:5} != 'Field' ]] 
+then 
+mysql --user="root" --password="Idem4546" --database="monitor" --execute= -e "ALTER TABLE dispositifs CHANGE materiel mat_json text;"
+fi
