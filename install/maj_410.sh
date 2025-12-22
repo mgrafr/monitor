3108bb69a0716c39491a95b0acb3ce82059e0f73
domo-site <93765654+mgrafr@users.noreply.github.com>
2025-12-22 21:22:04 +0100

    Add files via upload

---
+++ install/maj_410.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+cd /www/monitor
+if [ ! -d "vendor/php-mqtt" ]; then
+apt install composer
+composer require php-mqtt/client
+fi
+mkdir tmp
+cd tmp
+wget https://github.com/mgrafr/monitor/archive/refs/tags/monitor-v4.1.0.tar.gz
+tar -xzf monitor-v4.0.1.tar.gz
+cp -u ajax.php /www/monitor/ajax.php
+cp -u fonctions.php /www/monitor/fonctions.php
+cp -u ColorConverter.php /www/monitor/ColorConverter.php
+cp -u index_loc.php /www/monitor/index_loc.php
+cp -u -R include/* /www/monitor/include/
+cp -u css/* /www/monitor/css/
+cp -u js/* /www/monitor/js/
+cp -u api/* /www/monitor/api/
+cp -u images/* /www/monitor/images/
+cp -u -R install/* /www/monitor/install/
+cp -u -R bd_sql/* /www/monitor/bd_sql/
+cp -u -R share/* /www/monitor/share/
+cp -u .version /www/monitor/.version
+cd ..
+rm -R tmp
+result()(mysql --user="root" --password="Idem4546" --database="monitor" --execute= -e "SHOW COLUMNS FROM dispositifs LIKE 'materiel';") 
+res=${result} 
+if [[ ${res:0:5} != 'Field' ]] 
+then 
+mysql --user="root" --password="Idem4546" --database="monitor" --execute= -e "ALTER TABLE dispositifs CHANGE materiel param text;"
+fi
