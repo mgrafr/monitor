3e324f3488773630635d208c53cfd2fefb2e28a3
domo-site <93765654+mgrafr@users.noreply.github.com>
2026-01-08 18:34:15 +0100

    Add files via upload

--- include/iobroker.php
+++ include/iobroker.php
@@ -12,7 +12,7 @@ if ($domaine==IPMONITOR) {$lien_iobroker='http://'.$IP_iob.':'.$port_webui_iob;
 <div id="iobroker" >
 	<div class="container">
 		<div class="col-md-12">
-	  <h1 id="about" class="title" >IO BROKER :
+	  <h1 id="about_iob" class="title" >IO BROKER :
 		  <span style="color:blue">io.broker</span></h1>
 			<input type="button" value="refresh" onclick="refreshIframe()"/>
 			<p><a href="<?php echo $lien_iobroker1;?>" target="_blank" title="io.broker">
