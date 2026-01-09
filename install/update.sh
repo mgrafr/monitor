7ea2bf49c1d6d6a3480d0903d8053dec43689e43
domo-site <93765654+mgrafr@users.noreply.github.com>
2026-01-09 22:52:52 +0100

    Update update.sh

--- install/update.sh
+++ install/update.sh
@@ -25,13 +25,15 @@ if [ "$vermon" = "Version 411" ] && [ "$answer" -gt 324 ] && [ "$answer" -ne 411
     wget https://raw.githubusercontent.com/mgrafr/monitor/main/install/maj_410.sh
     dos2unix maj_411.sh
     chmod +x maj_411.sh 
-    ./maj_410.sh
+    ./maj_411.sh
+    rm maj_411.sh
  elif [ "$vermon" =  "Version 4.1.1" ] && [ "$answer" -eq 324 ]; then 
     echo "mise a jour vers 4.1.1"
     wget https://raw.githubusercontent.com/mgrafr/monitor/main/install/maj_324.sh
     dos2unix maj_324.sh
     chmod +x maj_324.sh 
     ./maj_324.sh
+    rm maj_324.sh
  elif [ "$vermon" =  "Version 4.1.1" ] && [ "$answer" -eq 411 ]; then  
      echo "deja a jour"
  elif [ "$vermon" = "Version 4.1.1" ] && [ "$answer" -lt 324 ] && [ "$answer" -gt 322 ]; then 
@@ -40,6 +42,7 @@ if [ "$vermon" = "Version 411" ] && [ "$answer" -gt 324 ] && [ "$answer" -ne 411
     dos2unix maj_324.sh
     chmod +x maj_324.sh 
     ./maj_324.sh
+    rm maj_324.sh
  elif [ "$vermon" = "Version en dev" ] && [ "$answer" -lt 324 ] && [ "$answer" -gt 322 ]; then  
       echo "mettre à jour vers version 4.1.1" avant une mise à jour DEV     
  elif [ "$vermon" = "Version en dev" ] && [ "$answer" -gt 324 ] && [ "$answer" -lt 411 ] ; then  
@@ -48,6 +51,7 @@ if [ "$vermon" = "Version 411" ] && [ "$answer" -gt 324 ] && [ "$answer" -ne 411
       dos2unix maj_dev.sh
       chmod +x maj_dev.sh 
       ./maj_dev.sh
+      rm maj_dev.sh
  else  
     echo "faire maj complete"
  fi
