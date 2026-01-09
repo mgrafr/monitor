a7cde12ea47d36fbdcaf97410d41e8d77ffaca40
domo-site <93765654+mgrafr@users.noreply.github.com>
2026-01-09 15:33:33 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import sphinx_rtd_theme
 project = 'monitor-domotique'
 copyright = '2026, Gravier'
 author = 'Michel Gravier'
-release = '4.1.0'
+release = '4.1.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
