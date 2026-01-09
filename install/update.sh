d5e296a10f102aae2d49ac80802217c915ebf252
domo-site <93765654+mgrafr@users.noreply.github.com>
2026-01-09 16:35:07 +0100

    Update update.sh

--- install/update.sh
+++ install/update.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/bash
 
 cd /www/monitor
-rm install/update.sh
-rm install/maj*
+rm maj*
 version=$(head -n 1 .version)
 echo $version
 vermon=$(whiptail --title "version de monitor" --radiolist \
