660f374a165ae7efb03bda9858815b7a28ccc18f
domo-site <93765654+mgrafr@users.noreply.github.com>
2026-01-09 16:42:40 +0100

    Update update-monitor.sh

--- install/update-monitor.sh
+++ install/update-monitor.sh
@@ -1,4 +1,5 @@
-rm /www/monitor/install/update.sh
+cd /www/monitor
+rm update.sh
 wget https://raw.githubusercontent.com/mgrafr/monitor/refs/heads/main/install/update.sh
 chmod +x update.sh
 ./update.sh
