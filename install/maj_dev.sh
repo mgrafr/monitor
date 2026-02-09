8aed5f50e4e7b323fc6a03828b8d7497c9944761
domo-site <93765654+mgrafr@users.noreply.github.com>
2026-02-09 10:08:24 +0100

    Update maj_dev.sh

--- install/maj_dev.sh
+++ install/maj_dev.sh
@@ -5,6 +5,15 @@ mkdir -p tmp
 cd tmp
 git clone https://github.com/mgrafr/monitor.git
 cd monitor
+# maj 4.1.3
+sed -i "s/: ws=9001 wss=9002 ou 9883/=9001   pour anciennes versions de Monitor***/" admin/config.php
+sed -i "/pour anciennes versions de Monitor/a \define('MQTT_PORTS', array(\n'mqtt' => '1883', \n'ws' => '9001', \n'mqtts' => '8883',\n'wss' => '443'));" admin/config.php
+sed -i "s/=> '1883',/=> '1883',\/\/ MQTT/" admin/config.php
+sed -i "s/=> '9001',/=> '9001',\/\/ Websocket/" admin/config.php
+sed -i "s/=> '8883',/=> '8883',\/\/ MQTTS/" admin/config.php
+sed -i "s/=> '443'));/'443'));\/\/ WSS pour Letsencrypt laisser 443,/" admin/config.php
+sed -i "/pour Letsencrypt laisser 443/a \ \/\/ COURTIER ET NGINX DOIVENT ECOUTER LE MEME PORT ex:9002" admin/config.php
+# -----------------------------------
 cp -u ajax.php /www/monitor/ajax.php
 cp -u fonctions.php /www/monitor/fonctions.php
 cp -u index_loc.php /www/monitor/index_loc.php
