e34b97c06948657e9d84072bc540212e1e132dda
domo-site <93765654+mgrafr@users.noreply.github.com>
2026-05-16 09:11:57 +0200

    Update update.sh

--- install/update.sh
+++ install/update.sh
@@ -6,7 +6,7 @@ version=$(head -n 1 .version)
 echo $version
 vermon=$(whiptail --title "version de monitor" --radiolist \
 "Vers quelle version voulez vous mettre à jour ?\n la version en développement\n ou la version LATEST " 15 60 4 \
-"Version 4.1.1" "par defaut " ON \
+"Version 4.2.0" "par defaut " ON \
 "Version en dev" "voir la doc" OFF 3>&1 1>&2 2>&3)
 exitstatus=$?
 # ------------------------------
@@ -20,32 +20,32 @@ echo $answer
 apt update
 apt install dos2unix
 apt install unzip
-if [ "$vermon" = "Version 411" ] && [ "$answer" -gt 324 ] && [ "$answer" -ne 411 ]; then
-    echo "mise a jour vers 4.1.1"
+if [ "$vermon" = "Version 420" ] && [ "$answer" -gt 324 ] && [ "$answer" -ne 420 ]; then
+    echo "mise a jour vers 4.2.0"
     wget https://raw.githubusercontent.com/mgrafr/monitor/main/install/maj_410.sh
-    dos2unix maj_411.sh
-    chmod +x maj_411.sh 
-    ./maj_411.sh
-    rm maj_411.sh
- elif [ "$vermon" =  "Version 4.1.1" ] && [ "$answer" -eq 324 ]; then 
-    echo "mise a jour vers 4.1.1"
+    dos2unix maj_420.sh
+    chmod +x maj_420.sh 
+    ./maj_420.sh
+    rm maj_420.sh
+ elif [ "$vermon" =  "Version 4.2.0" ] && [ "$answer" -eq 324 ]; then 
+    echo "mise a jour vers 4.2.0"
     wget https://raw.githubusercontent.com/mgrafr/monitor/main/install/maj_324.sh
     dos2unix maj_324.sh
     chmod +x maj_324.sh 
     ./maj_324.sh
     rm maj_324.sh
- elif [ "$vermon" =  "Version 4.1.1" ] && [ "$answer" -eq 411 ]; then  
+ elif [ "$vermon" =  "Version 4.2.0" ] && [ "$answer" -eq 420 ]; then  
      echo "deja a jour"
- elif [ "$vermon" = "Version 4.1.1" ] && [ "$answer" -lt 324 ] && [ "$answer" -gt 322 ]; then 
-    echo "mise a jour vers 4.1.1"
+ elif [ "$vermon" = "Version 4.2.0" ] && [ "$answer" -lt 324 ] && [ "$answer" -gt 322 ]; then 
+    echo "mise a jour vers 4.2.0"
     wget https://raw.githubusercontent.com/mgrafr/monitor/main/install/maj_324.sh
     dos2unix maj_324.sh
     chmod +x maj_324.sh 
     ./maj_324.sh
     rm maj_324.sh
  elif [ "$vermon" = "Version en dev" ] && [ "$answer" -lt 324 ] && [ "$answer" -gt 322 ]; then  
-      echo "mettre à jour vers version 4.1.1" avant une mise à jour DEV     
- elif [ "$vermon" = "Version en dev" ] && [ "$answer" -gt 324 ] && [ "$answer" -lt 413 ] ; then  
+      echo "mettre à jour vers version 4.2.0" avant une mise à jour DEV     
+ elif [ "$vermon" = "Version en dev" ] && [ "$answer" -gt 324 ] && [ "$answer" -lt 421 ] ; then  
       echo "mise à jour vers version dev"  
       wget https://raw.githubusercontent.com/mgrafr/monitor/main/install/maj_dev.sh
       dos2unix maj_dev.sh
