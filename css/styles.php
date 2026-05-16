7cd2dd4d349aab0f82b731ea979f2dcfd17ea847
domo-site <93765654+mgrafr@users.noreply.github.com>
2026-05-16 22:54:01 +0200

    Add files via upload

--- css/styles.php
+++ css/styles.php
@@ -6,4 +6,5 @@ echo '
 .banner-image {vertical-align: middle;min-height: 100%;width: 100%;    height: 830px;background-image: url('.IMAGEACCUEIL.');}    
 @media (max-width: 823px) {
     .banner-image {background-image: url('.IMAGEACCUEILSMALL.'); } }   
-@media (max-width:768px) {.banner-image {height:750px}}</style>';
+@media (max-width:768px) {.banner-image {height:750px}}
+@media (max-width: 823px) {.banner-image {height:930px}}</style>';
