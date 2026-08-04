f04ba68e2948b30df20dc96aab1b09470fe26ea9
domo-site <93765654+mgrafr@users.noreply.github.com>
2026-08-04 21:11:35 +0200

    Add files via upload

---
+++ include/html_recettes/14.php
@@ -0,0 +1,26 @@
+<style>
+.txt1_cuisine{
+margin-left: 10px;
+    font-size: 18px;
+    font-family: auto;
+font-style: oblique;}</style>
+
+<p ><strong>Gratin de courgettes</strong>&nbsp;<em></em><br>
+ingrédients pour 3 à 4 Personnes <p>	
+<div class="txt1_cuisine" style="margin-left:10px" ><img src="$$images/courgettes.webp" style="vertical-align: middle; width:60px;heght:auto" alt=""/>
+  &nbsp;&nbsp;&nbsp;<span class="txt1_cuisine">2 ou 3 courgettes</span></div>
+<div class="txt1_cuisine" style="margin-top: 5px;margin-left:10px"><img src="$$images/oeufs.webp" style="vertical-align: middle; width:60px;heght:auto" alt=""/>
+  &nbsp;&nbsp;&nbsp;&nbsp;1 ou 2 oeufs</div>
+<div class="txt1_cuisine" style="margin-left:10px"><img src="$$images/creme-fraiche.webp" style="vertical-align: middle;width:60px;heght:auto" alt=""/>
+  &nbsp;&nbsp;&nbsp;20 25 cl de crème fraiche</div>
+<div class="txt1_cuisine" style="margin-left:10px"><img src="$$images/gruyere.webp" style="vertical-align: middle;width:60px;heght:auto" alt=""/>
+	&nbsp;&nbsp;&nbsp;150-200 g de Gruyère rapé</div>
+<div class="txt1_cuisine" style="margin-left:10px"><img src="$$images/sel_poivre.webp" style="vertical-align: middle;width:60px;heght:auto" alt=""/>
+	&nbsp;&nbsp;&nbsp;2 pincées de sel, 1 pincée de poivre</div>
+<p> <strong>Préparation :</strong><br>- Laver les courgettes, les éplucher en laissant des petites bandes de peau<br>
+	- Couper les en rondelles. <br>- Faire bouillir de l'eau saléedans une casserolle et plogerz-y les courgette pendan 5 minutes.
+	<br>- Pendant ce temps, mélanger l'(les)oeuf(s) la crème fraiche, 150 g de gruyère rapé, le sel et le poivre.</p>
+<p> <strong>Cuisson :</strong>	
+	<br>- Préchauffer le four à 200°. <br>- Egouter les courgetteset les disposer dans un plat à gratin. 
+	<br>- Parsemer le dessus du reste de gruyère rapé. <br>- Enfourner pour 25 minutes.
+	</p>
